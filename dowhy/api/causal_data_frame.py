9b25a1b7f66d0b019fac18cf26db60f3216aab43
Adam Kelleher <akelleh@gmail.com>
2019-02-20 15:41:43 -0500

    pep8

--- dowhy/api/causal_data_frame.py
+++ dowhy/api/causal_data_frame.py
@@ -26,26 +26,26 @@ class CausalAccessor(object):
             self.reset()
         if not self._causal_model:
             self._causal_model = CausalModel(self._obj,
-                                                  [xi for xi in x.keys()][0],
-                                                  outcome,
-                                                  graph=dot_graph,
-                                                  common_causes=common_causes,
-                                                  instruments=instruments,
-                                                  estimand_type=estimand_type,
-                                                  proceed_when_unidentifiable=proceed_when_unidentifiable)
+                                             [xi for xi in x.keys()][0],
+                                             outcome,
+                                             graph=dot_graph,
+                                             common_causes=common_causes,
+                                             instruments=instruments,
+                                             estimand_type=estimand_type,
+                                             proceed_when_unidentifiable=proceed_when_unidentifiable)
         self._identified_estimand = self._causal_model.identify_effect()
         if not self._sampler:
             self._method = method
             do_sampler_class = do_samplers.get_class_object(method + "_sampler")
             self._sampler = do_sampler_class(self._obj,
-                                                  self._identified_estimand,
-                                                  self._causal_model._treatment,
-                                                  self._causal_model._outcome,
-                                                  params=params,
-                                                  variable_types=variable_types,
-                                                  num_cores=num_cores,
-                                                  causal_model=self._causal_model,
-                                                  keep_original_treatment=keep_original_treatment)
+                                             self._identified_estimand,
+                                             self._causal_model._treatment,
+                                             self._causal_model._outcome,
+                                             params=params,
+                                             variable_types=variable_types,
+                                             num_cores=num_cores,
+                                             causal_model=self._causal_model,
+                                             keep_original_treatment=keep_original_treatment)
         result = self._sampler.do_sample(x)
         if not stateful:
             self.reset()
