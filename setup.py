28cf381675f218d8a13f424322f7c3942a9ad136
Amit Sharma <amit_sharma@live.com>
2019-07-15 18:12:13 +0530

    commits to publish to pypi

--- setup.py
+++ setup.py
@@ -22,18 +22,13 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 setup(
     name='dowhy',
-
-    version='0.1.0',
-
+    version='0.1.2',
     description='A Python library for causal inference',  # Required
-
+    license='MIT',
     long_description=long_description,
-
     url='https://github.com/microsoft/dowhy',  # Optional
-
+    download_url='https://github.com/microsoft/dowhy/archive/v0.1-alpha.tar.gz',
     author='Amit Sharma, Emre Kiciman',
-
-
     classifiers=[  # Optional
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
@@ -46,10 +41,8 @@ setup(
     ],
 
     keywords='causality machine-learning causal-inference statistics graphical-model',
-
     packages=find_packages(exclude=['docs', 'tests']),
     python_requires='>=3.0',
-
-    install_requires=install_requires
-
+    install_requires=install_requires,
+    include_package_data=True
 )
