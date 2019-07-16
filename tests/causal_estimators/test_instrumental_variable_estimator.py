52aea4acd941c01d316374bd3a8ddb1a623c1ec4
Amit Sharma <amit_sharma@live.com>
2019-07-16 17:49:22 +0530

    minor updates to the test code

--- tests/causal_estimators/test_instrumental_variable_estimator.py
+++ tests/causal_estimators/test_instrumental_variable_estimator.py
@@ -4,10 +4,10 @@ from dowhy.causal_estimators.instrumental_variable_estimator import Instrumental
 from .base import TestEstimator
 
 
-class TestPropensityScoreMatchingEstimator(object):
+class TestInstrumentalVariableEstimator(object):
     @pytest.mark.parametrize(["error_tolerance", "Estimator"],
-                             [(0.05, InstrumentalVariableEstimator),
-                              (0.1, InstrumentalVariableEstimator)])
+                             [(0.1, InstrumentalVariableEstimator),
+                              (0.2, InstrumentalVariableEstimator)])
     def test_average_treatment_effect(self, error_tolerance, Estimator):
         estimator_tester = TestEstimator(error_tolerance, Estimator)
         estimator_tester.average_treatment_effect_test()
