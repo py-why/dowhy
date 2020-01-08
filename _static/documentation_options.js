10ac587f366285c08e3b7a5c536c8e5551cc4959
Amit Sharma <amit_sharma@live.com>
2020-01-08 13:10:29 +0530

    rebuilt docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
