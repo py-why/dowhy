ba5d016a97b2ac0f49496e4b8357c0d517427f3d
Tanmay Kulkarni <tanmaygk97@gmail.com>
2020-02-25 17:44:50 +0530

    Add bootstrap refuter (#111)
    * Add bootstrap refuter
    
    * Update documentation and add return type
    
    * fix missing function call
    
    * Add missing import
    
    * remove unecessary indent
    
    * refactor code to accomodate multiple samples
    
    * fix mismatch in number_of_samples and sample_size
    
    * fix indent error
    
    * Add documentation for sample_size

---
+++ dowhy/causal_refuters/bootstrap_refuter.py
@@ -0,0 +1,57 @@
+from dowhy.causal_refuter import CausalRefuter, CausalRefutation
+import numpy as np
+from sklearn.utils import resample
+import logging
+
+class BootstrapRefuter(CausalRefuter):
+    """
+    Refute an estimate by running it on a random sample of the original data.
+    It supports additional parameters that can be specified in the regute_estimate() method.
+    - 'number_of_samples': int, None by default
+    The number of bootstrap samples to be constructed
+    - 'sample_size': int, None by default
+    The size of each bootstrap sample
+    - 'random_state': int, RandomState, None by default
+    The seed value to be added if we wish to repeat the same random behavior. For this purpose, 
+    we repeat the same seed in the psuedo-random generator.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._number_of_samples = kwargs.pop("number_of_samples", 200)
+        self._sample_size = kwargs.pop("sample_size",len(self._data))
+        self._random_state = kwargs.pop("random_state",None)
+
+        if 'logging_level' in kwargs:
+            logging.basicConfig(level=kwargs['logging_level'])
+        else:
+            logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        
+    def refute_estimate(self, *args, **kwargs):
+        if self._sample_size > len(self._data):
+                self.logger.warning("The sample size is larger than the population size")
+
+        sample_estimates = np.zeros(self._number_of_samples) 
+        
+        for index in range( self._number_of_samples ):
+            if self._random_state is None:
+                new_data = resample(self._data, 
+                                n_samples=self._sample_size )
+            else:
+                new_data = resample(self._data,
+                                    n_samples=self._sample_size,
+                                    random_state=self._random_state )
+
+            new_estimator = self.get_estimator_object(new_data, self._target_estimand, self._estimate)
+            new_effect = new_estimator.estimate_effect()
+            sample_estimates[index] = new_effect.value
+
+        refute = CausalRefutation(
+            self._estimate.value,
+            np.mean(sample_estimates),
+            refutation_type="Refute: Bootstrap Sample Dataset"
+        )
+
+        return refute
+
