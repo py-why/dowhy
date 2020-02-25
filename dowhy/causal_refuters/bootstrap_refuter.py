c67d9c473d7fb090f046e991fec56550ca02e4fd
Tanmay Kulkarni <tanmaygk97@gmail.com>
2020-02-25 13:50:58 +0530

    Update documentation and add return type

--- dowhy/causal_refuters/bootstrap_refuter.py
+++ dowhy/causal_refuters/bootstrap_refuter.py
@@ -5,9 +5,11 @@ class BootstrapRefuter(CausalRefuter):
     """
     Refute an estimate by running it on a random sample of the original data.
     It supports additional parameters that can be specified in the regute_estimate() method.
-    - 'number_of_sumples': The number of bootstrap samples to be constructed
-    - 'random_state': The seed value to be added if we wish to repeat the same random behavior. For this
-    purpose, we repeat the same seed in the psuedo-random generator.
+    - 'number_of_sumples': int, None by default
+    The number of bootstrap samples to be constructed
+    - 'random_state': int, RandomState, None by default
+    The seed value to be added if we wish to repeat the same random behavior. For this purpose, 
+    we repeat the same seed in the psuedo-random generator.
     """
 
     def __init__(self, *args, **options):
@@ -35,4 +37,6 @@ class BootstrapRefuter(CausalRefuter):
                 np.mean(sample_estimates),
                 refutation_type="Refute: Bootstrap Sample Dataset"
             )
+
+            return refute
 
