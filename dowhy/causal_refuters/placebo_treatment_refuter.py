36580133f3ddf744647e8227eacc62d76b5cc0e7
Tanmay Kulkarni <tanmaygk97@gmail.com>
2020-02-27 15:46:49 +0530

    Create multiple samples for Placebo Refuter
        The user can now run the subset refuter multiple times, so as to help generate confidance intervals for the value of the refuter obtained.
         The list of changes include:
        - Add logging
        - Add RandomState
        - Create Array of Refutations
        - Update inline documentation to reflect changes

--- dowhy/causal_refuters/placebo_treatment_refuter.py
+++ dowhy/causal_refuters/placebo_treatment_refuter.py
@@ -11,27 +11,55 @@ class PlaceboTreatmentRefuter(CausalRefuter):
 
     Supports additional parameters that can be specified in the refute_estimate() method.
 
-    - '_placebo_type': Default is to generate random values for the treatment. If placebo_type is "permute", then the original treatment values are permuted by row.
-
+    - 'placebo_type':  str, None by default
+    Default is to generate random values for the treatment. If placebo_type is "permute", 
+    then the original treatment values are permuted by row.
+    - 'number_of_samples': int, None by default
+    The number of samples to be constructed
+    - 'random_state': int, RandomState, None by default
+    The seed value to be added if we wish to repeat the same random behavior. If we with to repeat the
+    same behavior we push the same seed in the psuedo-random generator
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._placebo_type = kwargs["placebo_type"]
+        self._placebo_type = kwargs.pop("placebo_type",None)
+        self._number_of_samples = kwargs.pop("number_of_samples",200)
+        self._random_state = kwargs.pop("random_state",None)
 
     def refute_estimate(self):
-        num_rows = self._data.shape[0]
-        if self._placebo_type == "permute":
-            new_treatment = self._data[self._treatment_name].sample(frac=1).values
-        else:
-            new_treatment = np.random.randn(num_rows)
-        new_data = self._data.assign(placebo=new_treatment)
 
-        self.logger.debug(new_data[0:10])
+        # We need to change the identified estimand
+        # This is done as a safety measure, we don't want to change the
+        # original DataFrame
         identified_estimand = copy.deepcopy(self._target_estimand)
         identified_estimand.treatment_variable = ["placebo"]
 
-        new_estimator = self.get_estimator_object(new_data, identified_estimand, self._estimate)
-        new_effect = new_estimator.estimate_effect()
-        refute = CausalRefutation(self._estimate.value, new_effect.value,
+        sample_estimates = np.zeros(self._number_of_samples)
+        num_rows = self._data.shape[0]
+
+        for index in range( self._number_of_samples ):
+
+            if self._placebo_type == "permute":
+                if self._random_state is None:
+                    new_treatment = self._data[self._treatment_name].sample(frac=1).values
+                else:
+                    new_treatment = self._data[self._treatment_name].sample(frac=1, 
+                                                                    random_state=self._random_state).values                    
+            else:
+                new_treatment = np.random.randn(num_rows)
+            
+            # Create a new column in the data by the name of placebo
+            new_data = self._data.assign(placebo=new_treatment)
+
+            # Sanity check the data
+            self.logger.debug(new_data[0:10])
+
+            
+            new_estimator = self.get_estimator_object(new_data, identified_estimand, self._estimate)
+            new_effect = new_estimator.estimate_effect()
+            sample_estimates[index] = new_effect.value
+
+        refute = CausalRefutation(self._estimate.value, 
+                                  np.mean(sample_estimates),
                                   refutation_type="Refute: Use a Placebo Treatment")
         return refute
