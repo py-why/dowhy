96583ed1c4e2b0d1ef2da08aebe3ad2cd07288dd
Tanmay Kulkarni <tanmaygk97@gmail.com>
2020-02-27 12:08:22 +0530

    Create multiple samples for Subset Refuter
    The user can now run the subset refuter multiple times, so as to help generate confidance intervals for the value of the refuter obtained.
     The list of changes include:
    - Add logging
    - Add RandomState
    - Create Array of Refutations
    - Update inline documentation to reflect changes

--- dowhy/causal_refuters/data_subset_refuter.py
+++ dowhy/causal_refuters/data_subset_refuter.py
@@ -1,28 +1,47 @@
 from dowhy.causal_refuter import CausalRefuter, CausalRefutation
-
+import numpy as np
+import logging
 
 class DataSubsetRefuter(CausalRefuter):
     """Refute an estimate by rerunning it on a random subset of the original data.
 
     Supports additional parameters that can be specified in the refute_estimate() method.
 
-    - 'subset_fraction': Fraction of the data to be used for re-estimation.
-
+    - 'subset_fraction': float, None by default
+    Fraction of the data to be used for re-estimation.
+    - 'number_of_samples': int, None by default
+    The number of samples to be constructed
+    - random_state': int, RandomState, None by default
+    The seed value to be added if we wish to repeat the same random behavior. If we with to repeat the
+    same behavior we push the same seed in the psuedo-random generator
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._subset_fraction = kwargs["subset_fraction"]
+        self._subset_fraction = kwargs.pop("subset_fraction", 0.8)
+        self._number_of_samples = kwargs.pop("number_of_samples", 200)
+        self._random_state = kwargs.pop("random_state",None)
+
+        if 'logging_level' in kwargs:
+            logging.basicConfig(level=kwargs['logging_level'])
+        else:
+            logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
 
     def refute_estimate(self):
-        new_data = self._data.sample(frac=self._subset_fraction)
 
-        new_estimator = self.get_estimator_object(new_data, self._target_estimand, self._estimate)
-        new_effect = new_estimator.estimate_effect()
+        sample_estimates = np.zeros(self._number_of_samples)
+
+        for index in range( self._number_of_samples):
+            new_data = self._data.sample(frac=self._subset_fraction)
+
+            new_estimator = self.get_estimator_object(new_data, self._target_estimand, self._estimate)
+            new_effect = new_estimator.estimate_effect()
+            sample_estimates[index] = new_effect.value
 
         refute = CausalRefutation(
             self._estimate.value,
-            new_effect.value,
+            np.mean(sample_estimates),
             refutation_type="Refute: Use a subset of data"
         )
         return refute
