cf32812216d21a40346b9eaee7bc9ed184a4be27
Tanmay Kulkarni <tanmaygk97@gmail.com>
2020-03-13 12:47:14 +0530

    fix syntax error in subset refuter

--- tests/causal_refuters/test_data_subset_refuter.py
+++ tests/causal_refuters/test_data_subset_refuter.py
@@ -7,6 +7,6 @@ class TestDataSubsetRefuter(object):
     @pytest.mark.parameterize(["error_tolerance","estimator_method"],
                               [0.01, "iv.instrument_variable"])
     def test_refutation_placebo_refuter_continueous(self, error_tolerance, estimator_method):
-        refuter_tester = TestRefuter(error_tolerance, estimator_method, "data_subset_refuter"
-        refuter_tester.test_refutation_placebo_refuter_continueous_treatment_testsuite() # Run both
+        refuter_tester = TestRefuter(error_tolerance, estimator_method, "data_subset_refuter")
+        refuter_tester.continuous_treatment_testsuite() # Run both
         
