36b5f4b0efb3c519e0d0c3613aa67dda8c45cf07
Tanmay Kulkarni <tanmaygk97@gmail.com>
2020-04-06 12:43:23 +0530

    add functionality to call external causalml estimators in the estimate step (#124)
    * add causalml estimator
    
    * add hack to fix missing dependency
    
    * skip tests if install of causalml fails
    
    * add reason and make it run for the class
    
    * fix spelling mistake
    
    * add fixture for common source of data
    
    * rename the class
    
    * change variable name to make more semantic sense
    
    * clarify reasoning in comment
    
    * edit comment to clarify identification method used
    
    * remove redundant pdb
    
    * chain the imports
    
    * fix comments in code
    
    * clarify the need for 1D array in outcome and treatment
    
    * add TODO for symbolic estimator
    
    * remove redundant pdb debug statement
    
    * make init_data a class level fixture and account for ValueError in LREstimator
    
    * rename tests to match causalml regex
    
    * fix formatring in comment
    
    * remove instrumental variables from the list of Covariates for the estimator
    
    * fix invocation error
    
    * fix fixture errors
    
    * fix spellings in test function

---
+++ dowhy/causal_estimators/causalml.py
@@ -0,0 +1,106 @@
+import inspect
+import numpy as np
+import pandas as pd
+
+from dowhy.causal_estimator import CausalEstimate, CausalEstimator
+from importlib import import_module
+import causalml
+
+class Causalml(CausalEstimator):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        # Add the identification method used in the estimator
+        self.identifier_method = self._target_estimand.identifier_method
+        self.logger.debug("The identifier method used {}".format(self.identifier_method))
+
+        # Check the backdoor variables being used
+        self.logger.debug("Back-door variables used:" +
+                          ",".join(self._target_estimand.backdoor_variables))
+        
+        # Add the observed confounders and one hot encode the categorical variables
+        self._observed_common_causes_names = self._target_estimand.backdoor_variables
+        if self._observed_common_causes_names:
+            # Get the data of the unobserved confounders
+            self._observed_common_causes = self._data[self._observed_common_causes_names]
+            # One hot encode the data if they are categorical
+            self._observed_common_causes = pd.get_dummies(self._observed_common_causes, drop_first=True)
+        else:
+            self._observed_common_causes = []
+        
+        # Check the instrumental variables involved
+        self.logger.debug("Instrumental variables used:"+
+                        ",".join(self._target_estimand.instrumental_variables))
+        
+        # Perform the same actions as the above
+        self._instrumental_variable_names = self._target_estimand.instrumental_variables
+        if self._instrumental_variable_names:
+            self._instrumental_variables = self._data[self._instrumental_variable_names]
+            self._instrumental_variables = pd.get_dummies(self._instrumental_variables, drop_first=True)
+        else:
+            self._instrumental_variables = []
+
+        # Check if effect modifiers are used
+        self.logger.debug("Effect Modifiers used:" + 
+                        ",".join(self._effect_modifier_names))
+        
+        
+        # Get the class corresponding the the estimator to be used
+        estimator_class = self._get_causalml_class_object(self._causalml_methodname)
+        # Initialize the object
+        self.estimator = estimator_class(**self.method_params["init_params"])
+        self.logger.info("INFO: Using CausalML Estimator")
+        self.symbolic_estimator = self.construct_symbolic_estimator(self._target_estimand)
+        self.logger.info(self.symbolic_estimator)
+        
+    def _get_causalml_class_object(self, module_method_name, *args, **kwargs):
+        
+        try:
+            (module_name, _, class_name) = module_method_name.rpartition(".")
+            estimator_module = import_module(module_name)
+            estimator_class = getattr(estimator_module, class_name)
+
+        except (AttributeError, AssertionError, ImportError):
+            raise ImportError('Error loading {}.{}. Double-check the method name and ensure that all econml dependencies are installed.'.format(module_name, class_name))
+        return estimator_class
+
+    def _estimate_effect(self):
+        X_names = self._observed_common_causes_names + \
+                self._effect_modifier_names
+        
+        # Both the outcome and the treatment have to be 1D arrays according to the CausalML API
+        y_name = self._outcome_name 
+        treatment_name = self._treatment_name[0] # As we have only one treatment variable
+        # We want to pass 'v0' rather than ['v0'] to prevent a shape mismatch
+        
+        func_args={
+            'X':self._data[X_names],
+            'y':self._data[y_name],
+            'treatment':self._data[treatment_name]
+        }
+
+        arg_names = inspect.getfullargspec(self.estimator.estimate_ate)[0]
+        matched_args = {
+            arg: func_args[arg] for arg in func_args.keys() if arg in arg_names 
+        }
+        value_tuple = self.estimator.estimate_ate(**matched_args) 
+
+        estimate = CausalEstimate(estimate=value_tuple[0],
+                                  target_estimand=self._target_estimand,
+                                  realized_estimand_expr=self.symbolic_estimator,
+                                  effect_intervals=(value_tuple[1],value_tuple[2]),
+                                  _estimator_object=self.estimator)
+
+        return estimate
+        
+
+    def construct_symbolic_estimator(self, estimand):
+        expr = "b: " + ",".join(estimand.outcome_variable) + "~"
+        # TODO we are conditioning on a postive treatment
+        # TODO create an expression corresponding to each estimator used
+        var_list = estimand.treatment_variable + estimand.backdoor_variables
+        expr += "+".join(var_list)
+        return expr
+ 
---
+++ tests/causal_estimators/test_causalml_estimator.py
@@ -0,0 +1,194 @@
+import pytest
+import subprocess
+import sys
+
+from dowhy import CausalModel
+from dowhy.datasets import linear_dataset
+
+# To skip the tests if we cannot install causalml
+is_causalml_installed = True
+# Hack to install causalml if not already present
+try:
+    __import__("causalml")
+    # If it imports successfully the first time
+    from xgboost import XGBRegressor
+except ImportError:
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "causalml"])
+        __import__("causalml")
+        # We import XGBRegressor later as we obtained this from causalml
+        from xgboost import XGBRegressor
+    except subprocess.CalledProcessError:
+        is_causalml_installed = False
+
+@pytest.fixture
+def init_data():
+    data = linear_dataset(
+            beta=10,
+            num_common_causes=4,
+            num_instruments=2,
+            num_effect_modifiers=2,
+            num_treatments=1,
+            num_samples=1000,
+            treatment_is_binary=True
+        )
+    
+    return data
+
+@pytest.mark.skipif(is_causalml_installed is False, reason="CausalML is not installed")
+@pytest.mark.use_fixtures("init_data")
+class TestCausalmlEstimator:
+    '''
+        To test the basic functionality of the CausalML estimators
+    '''
+    
+    def test_causalml_LRSRegressor(self, init_data):
+        # Defined a linear dataset with a given set of properties
+        data = init_data
+
+        # Create a model that captures the same
+        model = CausalModel(
+            data=data['df'],
+            treatment=data['treatment_name'],
+            outcome=data['outcome_name'],
+            effect_modifiers=data['effect_modifier_names'],
+            graph=data['gml_graph']
+        )
+
+        # Identify the effects within the model
+        identified_estimand = model.identify_effect(
+            proceed_when_unidentifiable=True
+        )
+
+        lr_estimate = model.estimate_effect(
+            identified_estimand,
+            method_name="backdoor.causalml.inference.meta.LRSRegressor",
+            method_params={"init_params":{}}
+        )
+
+        print("The LR estimate obtained:")
+        print(lr_estimate)
+
+    def test_causalml_XGBTRegressor(self, init_data):
+        # Defined a linear dataset with a given set of properties
+        data = init_data
+
+        # Create a model that captures the same
+        model = CausalModel(
+            data=data['df'],
+            treatment=data['treatment_name'],
+            outcome=data['outcome_name'],
+            effect_modifiers=data['effect_modifier_names'],
+            graph=data['gml_graph']
+        )
+
+        # Identify the effects within the model
+        identified_estimand = model.identify_effect(
+            proceed_when_unidentifiable=True
+        )
+
+        xgbt_estimate = model.estimate_effect(
+            identified_estimand,
+            method_name="backdoor.causalml.inference.meta.XGBTRegressor",
+            method_params={"init_params":{}}
+        )
+
+        print("The XGBT estimate obtained:")
+        print(xgbt_estimate)
+
+    def test_causalml_MLPTRegressor(self, init_data):
+        # Defined a linear dataset with a given set of properties
+        data = init_data
+
+        # Create a model that captures the same
+        model = CausalModel(
+            data=data['df'],
+            treatment=data['treatment_name'],
+            outcome=data['outcome_name'],
+            effect_modifiers=data['effect_modifier_names'],
+            graph=data['gml_graph']
+        )
+
+        # Identify the effects within the model
+        identified_estimand = model.identify_effect(
+            proceed_when_unidentifiable=True
+        )
+
+        mlpt_estimate = model.estimate_effect(
+            identified_estimand,
+            method_name="backdoor.causalml.inference.meta.MLPTRegressor",
+            method_params={"init_params":{
+                    'hidden_layer_sizes':(10,10),
+                    'learning_rate_init':0.1,
+                    'early_stopping':True 
+                }
+            }
+        )
+
+        print("The MLPT estimate obtained:")
+        print(mlpt_estimate)
+
+    def test_causalml_XLearner(self, init_data):
+        # Defined a linear dataset with a given set of properties
+        data = init_data
+
+        # Create a model that captures the same
+        model = CausalModel(
+            data=data['df'],
+            treatment=data['treatment_name'],
+            outcome=data['outcome_name'],
+            effect_modifiers=data['effect_modifier_names'],
+            graph=data['gml_graph']
+        )
+
+        # Identify the effects within the model
+        identified_estimand = model.identify_effect(
+            proceed_when_unidentifiable=True
+        )
+
+        xl_estimate = model.estimate_effect(
+            identified_estimand,
+            method_name="backdoor.causalml.inference.meta.BaseXRegressor",
+            method_params={"init_params":{
+                    'learner':XGBRegressor()
+                }
+            }
+        )
+
+        print("The X Learner estimate obtained:")
+        print(xl_estimate)
+
+    def test_causalml_RLearner(self, init_data):
+        # Defined a linear dataset with a given set of properties
+        data = init_data
+
+        # Create a model that captures the same
+        model = CausalModel(
+            data=data['df'],
+            treatment=data['treatment_name'],
+            outcome=data['outcome_name'],
+            effect_modifiers=data['effect_modifier_names'],
+            graph=data['gml_graph']
+        )
+
+        # Identify the effects within the model
+        identified_estimand = model.identify_effect(
+            proceed_when_unidentifiable=True
+        )
+
+        rl_estimate = None
+
+        try:
+            rl_estimate = model.estimate_effect(
+                identified_estimand,
+                method_name="backdoor.causalml.inference.meta.BaseRRegressor",
+                method_params={"init_params":{
+                        'learner':XGBRegressor()
+                    }
+                }
+            )
+        except ValueError:
+            print("Error with respect to the number of samples")
+        
+        print("The R Learner estimate obtained:")
+        print(rl_estimate)
