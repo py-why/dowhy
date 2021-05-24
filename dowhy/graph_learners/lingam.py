026b41e9c6ae9350180aa807329d4eb8b703336b
siddhanthaldar <siddhanthaldar14@gmail.com>
2021-05-24 15:14:08 +0530

    Added lingam library to graph learners

--- dowhy/graph_learners/lingam.py
+++ dowhy/graph_learners/lingam.py
@@ -1,31 +1,26 @@
 import numpy as np
 import networkx as nx
-# import lingam
 
 from dowhy.graph_learners import GraphLearner
 from dowhy.utils.graph_operations import *
 
-# functions = {
-# 	'direct' : lingam.DirectLiNGAM,
-# 	'ica' : lingam.ICALiNGAM,
-# 	'bottom_up_parce' : lingam.BottomUpParceLiNGAM,
-# }
-
 class LINGAM(GraphLearner):
 
-	def __init__(self, data, method_name, *args, **kwargs):
-		super().__init__(data, method_name, *args, **kwargs)
+	def __init__(self, data, library_class, *args, **kwargs):
+		super().__init__(data, library_class, *args, **kwargs)
 		
-		if 'use_prior_knowledge' in kwargs and kwargs['use_prior_knowledge']:
-			from lingam.utils import make_prior_knowledge
-			pk = make_prior_knowledge(
-				n_variables=len(self._data.columns),
-				sink_variables=kwargs['sink_variables'])
-			# print(functions[method_name], method_name)
-			self._method = functions[method_name](prior_knowledge=pk)#, *args, **kwargs)
-			# self._method = lingam.DirectLiNGAM(prior_knowledge=pk, *args, **kwargs)
-		else:
-			self._method = functions[method_name](*args, **kwargs)
+		# if 'use_prior_knowledge' in kwargs and kwargs['use_prior_knowledge']:
+		# 	from lingam.utils import make_prior_knowledge
+		# 	pk = make_prior_knowledge(
+		# 		n_variables=len(self._data.columns),
+		# 		sink_variables=kwargs['sink_variables'])
+		# 	# print(functions[method_name], method_name)
+		# 	self._method = functions[method_name](prior_knowledge=pk)#, *args, **kwargs)
+		# 	# self._method = lingam.DirectLiNGAM(prior_knowledge=pk, *args, **kwargs)
+		# else:
+		# 	self._method = functions[method_name](*args, **kwargs)
+		
+		self._method = library_class(*args, **kwargs)
 
 	def learn_graph(self, labels=None):
 		'''
@@ -39,16 +34,8 @@ class LINGAM(GraphLearner):
 		if labels is not None:
 			self._labels = labels
 
-		graph_dot = adjacency_matrix_to_graph(self._adj_matrix, self._labels)
+		self._graph_dot = adjacency_matrix_to_graph(self._adj_matrix, self._labels)
 		
 		# Return in valid DOT format
-		graph_dot = str_to_dot(graph_dot.source)
-		return graph_dot
-
-	def _get_adjacency_matrix(self):
-		'''
-		Return the adjacency matrix of the graph
-		
-		'''
-		return self._adj_matrix	
-				
+		graph_str = str_to_dot(self._graph_dot.source)
+		return graph_str
