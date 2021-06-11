a480b2db0727485aa801a8579da0b2d8bcb1c76f
siddhanthaldar <siddhanthaldar14@gmail.com>
2021-06-11 11:11:15 +0530

    Added code for ID algorithm

--- dowhy/causal_identifiers/IDIdentifier.py
+++ dowhy/causal_identifiers/IDIdentifier.py
@@ -1,3 +1,6 @@
+from builtins import isinstance
+import warnings
+
 from networkx.algorithms.assortativity.pairs import node_attribute_xy
 import numpy as np
 import pandas as pd
@@ -5,10 +8,34 @@ import networkx as nx
 from queue import LifoQueue
 from ordered_set import OrderedSet
 
-from dowhy.causal_identifier import CausalIdentifier
+# from dowhy.causal_identifier import CausalIdentifier
 from dowhy.utils.api import parse_state
 
-class IDIdentifier: #(CausalIdentifier):
+class IDExpression:
+
+    def __init__(self):
+        self._product = []#OrderedSet()
+        self._sum = []#OrderedSet()
+    
+    def add_product(self, element):
+        self._product.append(element)
+        # self._product.add(element)
+    
+    def add_sum(self, element):
+        self._sum.append(element)
+        # self._sum.add(element)
+
+    def get_val(self, return_type):
+        """type = prod or sum"""
+        if return_type=='prod':
+            return self._product
+        elif return_type=="sum":
+            return self._sum
+        else:
+            raise Exception("Provide correct return type.")
+    
+
+class IDIdentifier:
 
     def __init__(self, treatment_names=None, outcome_names=None, causal_model=None):
         '''
@@ -20,8 +47,6 @@ class IDIdentifier: #(CausalIdentifier):
         :param causal_model: A CausalModel object.
         '''
 
-        # super().__init__(treatment_names=None, outcome_names=None, causal_model=None)
-        
         self._treatment_names = OrderedSet(parse_state(treatment_names))
         self._outcome_names = OrderedSet(parse_state(outcome_names))
         
@@ -29,20 +54,17 @@ class IDIdentifier: #(CausalIdentifier):
             raise Exception("A CausalModel object must be provided for ID identification algorithm.")
         else:
             self._adjacency_matrix = causal_model._graph.get_adjacency_matrix()
-            self._tsort_node_names = OrderedSet(list(nx.topological_sort(causal_model._graph._graph))) # topological sorting of graph nodes
+            try:
+                self._tsort_node_names = OrderedSet(list(nx.topological_sort(causal_model._graph._graph))) # topological sorting of graph nodes
+            except:
+                warnings.warn("Cannot find topological order")
             self._node_names = OrderedSet(causal_model._graph._graph.nodes)
-            # print("Original Ordering of nodes:", causal_model._graph._graph.nodes)
-            # print("Current ordering of nodes:", self._node_names)
-            # print("Adjacency Matrix:")
-            # print(self._adjacency_matrix)
         
         # Estimators list for returning after identification
         self._estimators = []
         self._estimator_set = set()
 
     def identify_effect(self, treatment_names=None, outcome_names=None, adjacency_matrix=None, node_names=None):
-        # print("Before Adjacency Matrix:")
-        # print(adjacency_matrix)
         if adjacency_matrix is None:
             adjacency_matrix = self._adjacency_matrix
         if treatment_names is None:
@@ -52,42 +74,28 @@ class IDIdentifier: #(CausalIdentifier):
         if node_names is None:
             node_names = self._node_names
         node2idx, idx2node = self._idx_node_mapping(node_names)
-        # print("####################################################################################")
-        # print("Before Line 1:", treatment_names, outcome_names, node_names)
-        # print("Adjacency Matrix:")
-        # print(adjacency_matrix)
-        # print(node2idx, idx2node)
 
         # Line 1
-        # print("Line 1")
         if len(treatment_names) == 0:
-            # print("Line 1 if")
+            identifier = IDExpression()
             estimator = {}
             estimator['outcome_vars'] = outcome_names
             estimator['condition_vars'] = OrderedSet()
-            estimator['marginalize_vars'] = node_names - outcome_names
-            # Check if estimator already added 
-            if not self._is_present(estimator):
-                self._estimators.append(estimator)
-                # print("Line 1 estimator:", estimator)
+            identifier.add_product(estimator)
+            identifier.add_sum(node_names - outcome_names)
+            self._estimators.append(identifier)
             return self._estimators
-        # exit()
 
         # Line 2 - Remove ancestral nodes that don't affect output
-        # print("Line 2")
         ancestors = self._find_ancestor(outcome_names, node_names, adjacency_matrix, node2idx, idx2node)
-        # print("Line 2 Ancestors:", ancestors)
         if len(node_names - ancestors) != 0: # If there are elements which are not the ancestor of the outcome variables
-            # print("Line 2 if")
             # Modify list of valid nodes
             treatment_names = treatment_names & ancestors
             node_names = node_names & ancestors
-            # node2idx, idx2node = self._idx_node_mapping(ancestors)
             adjacency_matrix = self._induced_graph(node_set=node_names, adjacency_matrix=adjacency_matrix, node2idx=node2idx)
             return self.identify_effect(treatment_names=treatment_names, outcome_names=outcome_names, adjacency_matrix=adjacency_matrix, node_names=node_names)
         
         # Line 3
-        # print("Line 3")
         # Modify adjacency matrix to obtain that corresponding to do(X)
         adjacency_matrix_do_x = adjacency_matrix.copy()
         for x in treatment_names:
@@ -97,65 +105,52 @@ class IDIdentifier: #(CausalIdentifier):
         ancestors = self._find_ancestor(outcome_names, node_names, adjacency_matrix_do_x, node2idx, idx2node)
         W = node_names - treatment_names - ancestors
         if len(W) != 0:
-            # print("Line 3 if")
             return self.identify_effect(treatment_names = treatment_names | W, outcome_names=outcome_names, adjacency_matrix=adjacency_matrix, node_names=node_names)
         
         # Line 4
-        # print("Line 4")
         # Modify adjacency matrix to remove treatment variables
         node_names_minus_x = node_names - treatment_names
         node2idx_minus_x, idx2node_minus_x = self._idx_node_mapping(node_names_minus_x)
         adjacency_matrix_minus_x = self._induced_graph(node_set=node_names_minus_x, adjacency_matrix=adjacency_matrix, node2idx=node2idx)
         c_components = self._find_c_components(adjacency_matrix=adjacency_matrix_minus_x, node_set=node_names_minus_x, idx2node=idx2node_minus_x)
-        # TODO: Take care of adding over v\(y union x)
         if len(c_components)>1:
-            # print("Line 4 if")
+            identifier = IDExpression()
             sum_over_set = node_names - (outcome_names | treatment_names)
             for component in c_components:
-                # print("Line 4 if For")
-                estimators = self.identify_effect(treatment_names=node_names-component, outcome_names=OrderedSet(list(component)), adjacency_matrix=adjacency_matrix, node_names=node_names)
-                for estimator in estimators:
-                    estimator['marginalize_vars'] |= sum_over_set
-                    # Check if estimator already added 
-                    if not self._is_present(estimator):
-                        self._estimators.append(estimator)
-                        # print("Line 4 estimator:", estimator)
+                expressions = self.identify_effect(treatment_names=node_names-component, outcome_names=OrderedSet(list(component)), adjacency_matrix=adjacency_matrix, node_names=node_names)
+                # estimators = self.identify_effect(treatment_names=node_names-component, outcome_names=OrderedSet(list(component)), adjacency_matrix=adjacency_matrix, node_names=node_names)
+                for expression in expressions:
+                    identifier.add_product(expression)
+            identifier.add_sum(sum_over_set)
+            self._estimators.append(identifier)
             return self._estimators
         
         # Line 5
-        # print("Line 5")
         S = c_components[0]
         c_components_G = self._find_c_components(adjacency_matrix=adjacency_matrix, node_set=node_names, idx2node=idx2node)
         if len(c_components_G)==1 and c_components_G[0] == node_names:
-            # print("Line 5 if")
             return ["FAIL"]
     
         # Line 6
-        # print("Line 6")
         if S in c_components_G:
-            # print("Line 6 if")
             sum_over_set = S - outcome_names
             prev_nodes = []
             for node in self._tsort_node_names:
                 if node in S:
-                    # print("Line 6 estimator node:", node)
+                    identifier = IDExpression()
                     estimator = {}
                     estimator['outcome_vars'] = OrderedSet([node])
                     estimator['condition_vars'] = OrderedSet(prev_nodes)
-                    estimator['marginalize_vars'] = sum_over_set
-                    # print("Line 6 estimator before if:", estimator)
-                    # Check if estimator already added 
-                    if not self._is_present(estimator):
-                        self._estimators.append(estimator)
-                        # print("Line 6 estimator:", estimator)
+                    identifier.add_product(estimator)
+                    identifier.add_sum(sum_over_set)
+                    # Check if estimator already added
+                    self._estimators.append(identifier) 
                 prev_nodes.append(node)
             return self._estimators
 
         # Line 7
-        # print("Line 7")
         for component in c_components_G:
             if S - component is None:
-                # print("Line 7 if")
                 return self.identify_effect(treatment_names=treatment_names & component, outcome_names=outcome_names, adjacency_matrix=self._induced_graph(node_set=component, adjacency_matrix=adjacency_matrix,node2idx=node2idx), node_names=node_names)
 
     def _find_ancestor(self, node_set, node_names, adjacency_matrix, node2idx, idx2node):
@@ -229,20 +224,37 @@ class IDIdentifier: #(CausalIdentifier):
             idx2node[i] = node
         return node2idx, idx2node
 
-    def _is_present(self, estimator):
+    # def _is_present(self, estimator):
+    #     string = ""
+    #     for node in estimator['outcome_vars']:
+    #         string += node
+    #     string += "|"
+    #     for node in estimator['condition_vars']:
+    #         string += node
+    #     string += "|"
+    #     for node in estimator['marginalize_vars']:
+    #         string += node
+    
+    #     if string not in self._estimator_set:
+    #         self._estimator_set.add(string)
+    #         return False
+    #     return True
+
+    def _print_estimator(self, estimator, level):
         string = ""
-        for node in estimator['outcome_vars']:
-            string += node
-        string += "|"
-        for node in estimator['condition_vars']:
-            string += node
-        string += "|"
-        for node in estimator['marginalize_vars']:
-            string += node
-    
-        if string not in self._estimator_set:
-            self._estimator_set.add(string)
-            return False
-        return True
-    
-    
+        if isinstance(estimator, IDExpression):
+            # string += "List:\n"
+            for i, expression in enumerate(estimator.get_val(return_type='prod')):
+                string += self._print_estimator(expression, level+1)
+        else:
+            for i in range(level):
+                string += "\t"
+            string += str(estimator)
+            string += "\n"
+        return string
+
+    def __str__(self):
+        string = ""
+        for estimator in self._estimators:
+            string += self._print_estimator(estimator, 0)
+        return string
