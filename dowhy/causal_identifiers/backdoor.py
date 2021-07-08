e86450be2cf124762755d055b1fae2617b5b97a9
siddhanthaldar <siddhanthaldar14@gmail.com>
2021-07-09 00:01:42 +0530

    Added class for Backdoor variable search

--- dowhy/causal_identifiers/backdoor.py
+++ dowhy/causal_identifiers/backdoor.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from dowhy.utils.graph_operations import adjacency_matrix_to_adjacency_list
 
 class NodePair:
     def __init__(self, node1, node2):
@@ -13,54 +14,57 @@ class Path:
         self.is_blocked = None # To store if path is blocked
         self.condition_vars = set() # To store variables needed to block the path
 
-def DFSUtil(graph, node1, node2, vis, path, paths):
-    path.append(node1)
-    vis.add(node1)
-    if node1 == node2:
-        paths.append(path.copy())
-    else:
-        for neighbour in graph[node1]:
-            if neighbour not in vis:
-                DFSUtil(graph, neighbour, node2, vis, path, paths)
-    path.pop()
-    vis.remove(node1)
+class Backdoor:
 
-def DFS(graph, node1, node2):
-    print("DFS")
-    vis = set()
-    paths = []
-    DFSUtil(graph, node1, node2, vis, [], paths)
-    return paths
-
-def adjacency_matrix_to_adjacency_list(adjacency_matrix, nodes=None):
-    adjlist = dict()
-    if nodes is None:
-        nodes = [str(i+1) for i in range(adjacency_matrix.shape[0])]
-    for i in range(adjacency_matrix.shape[0]):
-        adjlist[nodes[i]] = list()
-        for j in range(adjacency_matrix.shape[1]):
-            if adjacency_matrix[i, j] != 0:
-                adjlist[nodes[i]].append(nodes[j])
-    return adjlist
-
-def get_backdoor_paths(graph, nodes1, nodes2):
-    paths = []
-    graph = graph.to_directed()
-    undirected_graph = graph.to_undirected()
-    nodes12 = set(nodes1).union(nodes2)
+    def __init__(self, graph, nodes1, nodes2):
+        self._graph = graph
+        self._nodes1 = nodes1
+        self._nodes2 = nodes2
     
-    # Get adjacency list
-    adjlist = adjacency_matrix_to_adjacency_list(nx.to_numpy_matrix(undirected_graph), nodes=list(undirected_graph.nodes))
-    
-    for node1 in nodes1:
-        for node2 in nodes2:
-            paths = DFS(adjlist, node1, node2)
-            backdoor_paths = [
-                pth
-                for pth in paths
-                if graph.has_edge(pth[1], pth[0])]
-            filtered_backdoor_paths = [
-                pth
-                for pth in backdoor_paths
-                if len(nodes12.intersection(set(pth[1:-1])))==0]
+    def get_backdoor_paths(self):
+        undirected_graph = self._graph.to_undirected()
+        nodes12 = set(self._nodes1).union(self._nodes2)
         
+        # Get adjacency list
+        adjlist = adjacency_matrix_to_adjacency_list(nx.to_numpy_matrix(undirected_graph), labels=list(undirected_graph.nodes))
+        path_dict = {}
+
+        for node1 in self._nodes1:
+            for node2 in self._nodes2:
+                if (node1, node2) in path_dict:
+                    continue
+
+                paths = self._path_search(adjlist, node1, node2)
+                backdoor_paths = [
+                    pth
+                    for pth in paths
+                    if self._graph.has_edge(pth[1], pth[0])]
+                filtered_backdoor_paths = [
+                    pth
+                    for pth in backdoor_paths
+                    if len(nodes12.intersection(set(pth[1:-1])))==0]
+                
+                path_dict[(node1, node2)] = filtered_backdoor_paths
+            
+        return path_dict
+
+    def _path_search_util(self, graph, node1, node2, vis, path, paths):
+        path.append(node1)
+        vis.add(node1)
+        if node1 == node2:
+            paths.append(path.copy())
+        else:
+            for neighbour in graph[node1]:
+                if neighbour not in vis:
+                    self._path_search_util(graph, neighbour, node2, vis, path, paths)
+        path.pop()
+        vis.remove(node1)
+        return paths
+
+    def _path_search(self, graph, node1, node2):
+        '''
+        Path search using DFS.
+        '''
+        vis = set()
+        paths = self._path_search_util(graph, node1, node2, vis, [], [])
+        return paths
