3ad2b70915c86ad416342014a95b7c4605350a1b
siddhanthaldar <siddhanthaldar14@gmail.com>
2021-07-20 10:35:50 +0530

    Improved memoization

--- dowhy/causal_identifiers/backdoor.py
+++ dowhy/causal_identifiers/backdoor.py
@@ -6,51 +6,78 @@ class NodePair:
         self._node1 = node1
         self._node2 = node2
         self._is_blocked = None # To store if all paths between node1 and node2 are blocked
-        self._condition_vars = set() # To store variable to be conditioned on to block all paths between node1 and node2
+        self._condition_vars = [] #set() # To store variable to be conditioned on to block all paths between node1 and node2
+        self._complete = False # To store to paths between node pair have been completely explored.
     
-    def update(self, paths):
-        self._is_blocked = all([path.is_blocked() for path in paths])
-        if not self._is_blocked:
-            for path in paths:
-                self._condition_vars = self._condition_vars.union(path.get_condition_vars())
-        
+    def update(self, path, condition_vars=None):
+        if condition_vars is None:
+            '''path is a Path variable'''
+            if self._is_blocked is None:
+                self._is_blocked = path.is_blocked()
+            else:
+                self._is_blocked = self._is_blocked and path.is_blocked()
+            if not path.is_blocked():
+                # self._condition_vars = self._condition_vars.union(path.get_condition_vars())
+                self._condition_vars.append(path.get_condition_vars())
+
+        else:
+            '''path is a list'''
+            # self._condition_vars = self._condition_vars.union([*path[1:], *condition_vars])
+            self._condition_vars.append(set([*path[1:], *condition_vars]))
+            # self._condition_vars.append(set([*path, *condition_vars]))
+
+    # def update(self, paths):
+    #     self._is_blocked = all([path.is_blocked() for path in paths])
+    #     if not self._is_blocked:
+    #         for path in paths:
+    #             self._condition_vars = self._condition_vars.union(path.get_condition_vars())
+    
+    def get_condition_vars(self):
+        return self._condition_vars
+
+    def set_complete(self):
+        self._complete = True
+    
+    def is_complete(self):
+        return self._complete
+
     def __str__(self):
         string = ""
         string += "Blocked: " + str(self._is_blocked) + "\n"
         if not self._is_blocked:
-            string += "To block path, condition on: " + ",".join(self._condition_vars) + "\n"
+            condition_vars = [str(s) for s in self._condition_vars]
+            string += "To block path, condition on: " + ",".join(condition_vars) + "\n"
+            # string += "To block path, condition on: " + ",".join(self._condition_vars) + "\n"
         return string
     
 
 class Path:
     def __init__(self):
-        # self._path = list() # To store variables in the path
         self._is_blocked = None # To store if path is blocked
         self._condition_vars = set() # To store variables needed to block the path
+        # self._colliders = set() # Used to store colliders
     
     def update(self, path, is_blocked):
-        # self._path = path
+        '''
+        path is a list
+        '''
         self._is_blocked = is_blocked
         if not is_blocked:
             self._condition_vars = self._condition_vars.union(set(path[1:-1]))
     
     def is_blocked(self):
         return self._is_blocked
-
-    # def get_path(self):
-    #     return self._path
     
     def get_condition_vars(self):
         return self._condition_vars
     
     def __str__(self):
         string = ""
-        # string += "Path: " + ",".join(self._path) + "\n"
         string += "Blocked: " + str(self._is_blocked) + "\n"
         if not self._is_blocked:
             string += "To block path, condition on: " + ",".join(self._condition_vars) + "\n"
         return string
-        
+
 class Backdoor:
 
     def __init__(self, graph, nodes1, nodes2):
@@ -70,46 +97,56 @@ class Backdoor:
             for node2 in self._nodes2:
                 if (node1, node2) in path_dict:
                     continue
-                node_pair = NodePair(node1, node2)
-                paths = self._path_search(adjlist, node1, node2)
-                node_pair.update(paths)
-                path_dict[(node1, node2)] = node_pair
-                # path_dict[(node1, node2)] = paths
+                self._path_search(adjlist, node1, node2, path_dict)
         return path_dict
 
     def is_backdoor(self, path):
         return True if self._graph.has_edge(path[1], path[0]) else False
 
-    def _path_search_util(self, graph, node1, node2, vis, path, paths, is_blocked=False, prev_arrow=None):
+    def _path_search_util(self, graph, node1, node2, vis, path, path_dict, is_blocked=False, prev_arrow=None):
         '''
         :param prev_arrow: True if arrow incoming, False if arrow outgoing
         '''
         if is_blocked:
-            return paths
-        path.append(node1)
-        vis.add(node1)
-        if node1 == node2:
-            # Check if path is backdoor and does not have nodes1\node1 or nodes2\node2 as intermediate nodes
-            if self.is_backdoor(path) and len(self._nodes12.intersection(set(path[1:-1])))==0:
-                path_var = Path()
-                path_var.update(path.copy(), is_blocked)
-                paths.append(path_var)
-        else:
-            for neighbour in graph[node1]:
-                if neighbour not in vis:
-                    # True if arrow incoming, False if arrow outgoing
-                    next_arrow = False if self._graph.has_edge(node1, neighbour) else True 
-                    if next_arrow == True and prev_arrow == True:
-                        is_blocked = True
-                    paths = self._path_search_util(graph, neighbour, node2, vis, path, paths, is_blocked, not next_arrow) # Since incoming for current node is outgoing for the next
-        path.pop()
-        vis.remove(node1)
-        return paths
+            return
 
-    def _path_search(self, graph, node1, node2):
+        # If node pair has been fully explored
+        if ((node1, node2) in path_dict) and (path_dict[(node1, node2).is_complete()]):
+            for i in range(len(path)):
+                if (path[i], node2) not in path_dict:
+                    path_dict[(path[i], node2)] = NodePair(path[i], node2)
+                path_dict[(path[i], node2)].update(path[i:], path_dict[(node1, node2)].get_condition_vars())
+
+        else:
+            path.append(node1)
+            vis.add(node1)
+            if node1 == node2:
+                # Check if path is backdoor and does not have nodes1\node1 or nodes2\node2 as intermediate nodes
+                if self.is_backdoor(path) and len(self._nodes12.intersection(set(path[1:-1])))==0:
+                    for i in range(len(path)-1):
+                        if (path[i], node2) not in path_dict:
+                            path_dict[(path[i], node2)] = NodePair(path[i], node2)
+                        path_var = Path()
+                        path_var.update(path[i:].copy(), is_blocked)
+                        path_dict[(path[i], node2)].update(path_var)
+            else:
+                for neighbour in graph[node1]:
+                    if neighbour not in vis:
+                        # True if arrow incoming, False if arrow outgoing
+                        next_arrow = False if self._graph.has_edge(node1, neighbour) else True 
+                        if next_arrow == True and prev_arrow == True:
+                            is_blocked = True
+                        self._path_search_util(graph, neighbour, node2, vis, path, path_dict, is_blocked, not next_arrow) # Since incoming for current node is outgoing for the next
+            path.pop()
+            vis.remove(node1)
+
+        # Mark pair (node1, node2) complete
+        if (node1, node2) in path_dict:
+            path_dict[(node1, node2)].set_complete()
+
+    def _path_search(self, graph, node1, node2, path_dict):
         '''
         Path search using DFS.
         '''
         vis = set()
-        paths = self._path_search_util(graph, node1, node2, vis, [], [], False)
-        return paths
+        self._path_search_util(graph, node1, node2, vis, [], path_dict, is_blocked=False)
