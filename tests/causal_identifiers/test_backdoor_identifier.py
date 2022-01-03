8452f3948c39f17137dfd55c077e01b603e0ea18
Amit Sharma <amit_sharma@live.com>
2022-01-03 09:10:16 +0530

    Optimize backdoor criterion identification for large graphs (#342)
    * Used the d-separation algorithm from networkx
    * Updated backdoor identification to be faster
    * Refactor code for the new function
    * Changed the logic for unobserved confounding to simply remove the unobserved confounder when returning a backdoor set. Simplifies the logic.

--- tests/causal_identifiers/test_backdoor_identifier.py
+++ tests/causal_identifiers/test_backdoor_identifier.py
@@ -11,19 +11,19 @@ class TestBackdoorIdentification(object):
         graph = example_graph_solution.graph
         biased_sets = example_graph_solution.biased_sets
         identifier = CausalIdentifier(graph, "nonparametric-ate", method_name="exhaustive-search")
-        
+
         backdoor_results = identifier.identify_backdoor("X", "Y", include_unobserved=False)
         backdoor_sets = [
-            set(backdoor_result_dict["backdoor_set"]) 
+            set(backdoor_result_dict["backdoor_set"])
             for backdoor_result_dict in backdoor_results
             if len(backdoor_result_dict["backdoor_set"]) > 0
         ]
 
         assert (
             (len(backdoor_sets) == 0 and len(biased_sets) == 0) # No biased sets exist and that's expected.
-            or  
+            or
             all([
-                set(biased_backdoor_set) not in backdoor_sets 
+                set(biased_backdoor_set) not in backdoor_sets
                 for biased_backdoor_set in biased_sets
             ]) # No sets that would induce biased results are present in the solution.
         )
@@ -35,7 +35,7 @@ class TestBackdoorIdentification(object):
 
         backdoor_results = identifier.identify_backdoor("X", "Y", include_unobserved=False)
         backdoor_sets = [
-            set(backdoor_result_dict["backdoor_set"]) 
+            set(backdoor_result_dict["backdoor_set"])
             for backdoor_result_dict in backdoor_results
             if len(backdoor_result_dict["backdoor_set"]) > 0
         ]
@@ -46,18 +46,18 @@ class TestBackdoorIdentification(object):
         graph = example_graph_solution.graph
         expected_sets = example_graph_solution.minimal_adjustment_sets
         identifier = CausalIdentifier(graph, "nonparametric-ate", method_name="minimal-adjustment", proceed_when_unidentifiable=False)
-        
+
         backdoor_results = identifier.identify_backdoor("X", "Y", include_unobserved=False)
         backdoor_sets = [
-            set(backdoor_result_dict["backdoor_set"]) 
+            set(backdoor_result_dict["backdoor_set"])
             for backdoor_result_dict in backdoor_results
         ]
-        
+
         assert (
             ((len(backdoor_sets) == 0) and (len(expected_sets) == 0)) # No adjustments exist and that's expected.
             or
             all([
-                set(expected_set) in backdoor_sets 
+                set(expected_set) in backdoor_sets
                 for expected_set in expected_sets
             ])
         )
@@ -66,19 +66,19 @@ class TestBackdoorIdentification(object):
         graph = example_graph_solution.graph
         expected_sets = example_graph_solution.maximal_adjustment_sets
         identifier = CausalIdentifier(graph, "nonparametric-ate", method_name="maximal-adjustment", proceed_when_unidentifiable=False)
-        
+
         backdoor_results = identifier.identify_backdoor("X", "Y", include_unobserved=False)
-        
+
         backdoor_sets = [
-            set(backdoor_result_dict["backdoor_set"]) 
+            set(backdoor_result_dict["backdoor_set"])
             for backdoor_result_dict in backdoor_results
         ]
-        
+        print(backdoor_sets, expected_sets, example_graph_solution.graph_str)
         assert (
             ((len(backdoor_sets) == 0) and (len(expected_sets) == 0)) # No adjustments exist and that's expected.
             or
             all([
-                set(expected_set) in backdoor_sets 
+                set(expected_set) in backdoor_sets
                 for expected_set in expected_sets
             ])
         )
