95966f5bbd23048fcf813e60b2300db31fbfd1c1
Patrick Bloebaum <bloebp@amazon.com>
2022-06-01 22:40:40 +0200

    Introduce gcm uncertainty module
    Signed-off-by: Patrick Bloebaum <bloebp@amazon.com>

--- dowhy/gcm/_noise.py
+++ dowhy/gcm/_noise.py
@@ -1,9 +1,29 @@
+from typing import Any, Tuple, Optional, List, Callable
+
 import networkx as nx
+import numpy as np
 import pandas as pd
 
-from dowhy.gcm.cms import InvertibleStructuralCausalModel
-from dowhy.gcm.graph import get_ordered_predecessors, is_root_node, validate_causal_dag
-from dowhy.gcm.util.general import convert_numpy_array_to_pandas_column
+from dowhy.gcm.cms import InvertibleStructuralCausalModel, StructuralCausalModel
+from dowhy.gcm.fcms import PredictionModel
+from dowhy.gcm.graph import get_ordered_predecessors, is_root_node, validate_causal_dag, node_connected_subgraph_view
+from dowhy.gcm.util.general import convert_numpy_array_to_pandas_column, column_stack_selected_numpy_arrays, \
+    convert_to_data_frame, shape_into_2d
+
+
+def compute_data_from_noise(causal_model: StructuralCausalModel,
+                            noise_data: pd.DataFrame) -> pd.DataFrame:
+    result = {}
+    for node in nx.topological_sort(causal_model.graph):
+        if is_root_node(causal_model.graph, node):
+            result[node] = noise_data[node].to_numpy()
+        else:
+            result[node] = convert_numpy_array_to_pandas_column(
+                causal_model.causal_mechanism(node).evaluate(
+                    column_stack_selected_numpy_arrays(result, get_ordered_predecessors(causal_model.graph, node)),
+                    noise_data[node].to_numpy()))
+
+    return convert_to_data_frame(result)
 
 
 def compute_noise_from_data(causal_model: InvertibleStructuralCausalModel, observed_data: pd.DataFrame) -> pd.DataFrame:
@@ -20,3 +40,109 @@ def compute_noise_from_data(causal_model: InvertibleStructuralCausalModel, obser
                     observed_data[get_ordered_predecessors(causal_model.graph, node)].to_numpy()))
 
     return result
+
+
+def get_noise_dependent_function(causal_model: StructuralCausalModel,
+                                 target_node: Any,
+                                 approx_prediction_model: Optional[PredictionModel] = None,
+                                 num_training_samples: int = 20000) \
+        -> Tuple[Callable[[np.ndarray], np.ndarray], List[Any]]:
+    """Returns a function that represents the given target_node and that is only dependent on upstream noise nodes.
+    This is, Y = f(N_0, N_1, ..., N_n), where Y is the target node and N_i the noise node of an upstream node. Since
+    the order of the noise variables can be ambiguous, this method also returns a list with the expected order of the
+    noise variables indicated by the name of the corresponding node. For instance:
+    Lets say we have a target variable X4 which has only 2 other upstream variables X1 and X3, then this methods returns
+    a callable that represents X4 = f(N_1, N_3, N_4). This callable expects a numpy array as input. Since the order
+    of the columns in this array is unclear, the method also returns a list with the names of the corresponding columns,
+    e.g. ['X1', 'X3', 'X4']. Note that the noise of X4 will also be an input variable here.
+
+    If an approx_prediction_model is given, the model is used instead to train a model from scratch to represent f.
+    Typically, it would be much faster to evaluate this model than propagating through the graph. The model is trained
+    based on generated noise and target samples from the given causal graph. In theory, the approximated version of f
+    should be close to the "true" one based on the underlying SCMs, but there can still be (significant) differences,
+    especially if the provided model is inappropriate for representing f.
+
+    Note: All nodes in the graph that have no direct path to the target node are omitted. The noise node of the
+          target variable itself will also be included here.
+
+    :param causal_model: A structural causal model.
+    :param target_node: The target node for which the function f should be returned.
+    :param approx_prediction_model: Prediction model for approximating f. The model is trained based on drawn noise and
+                                    target samples.
+    :param num_training_samples: Number of drawn samples for training the predictor based on the provided
+                                 approx_prediction_model.
+                                 Note: This parameter is ignored if approx_prediction_model is None.
+    :return: A tuple, where the first value is a callable (the function f) that expects a numpy array X as input and
+    the second value is a list with nodes that represents the expected order of the columns in X.
+    """
+    validate_causal_dag(causal_model.graph)
+
+    if approx_prediction_model is not None:
+        return _get_approx_noise_dependent_function(
+            StructuralCausalModel(node_connected_subgraph_view(causal_model.graph, target_node)),
+            target_node,
+            approx_prediction_model,
+            num_training_samples)
+    else:
+        return _get_exact_noise_dependent_function(
+            StructuralCausalModel(node_connected_subgraph_view(causal_model.graph, target_node)),
+            target_node)
+
+
+def _get_exact_noise_dependent_function(causal_model: StructuralCausalModel,
+                                        target_node: Any) \
+        -> Tuple[Callable[[np.ndarray], np.ndarray], List[Any]]:
+    nodes_order = list(nx.topological_sort(causal_model.graph))
+
+    def predict_method(noise_samples: np.ndarray) -> np.ndarray:
+        return compute_data_from_noise(causal_model,
+                                       pd.DataFrame(noise_samples,
+                                                    columns=[x for x in nodes_order]))[target_node].to_numpy()
+
+    return predict_method, nodes_order
+
+
+def _get_approx_noise_dependent_function(causal_model: StructuralCausalModel,
+                                         target_node: Any,
+                                         approx_prediction_model: PredictionModel,
+                                         num_training_samples: int) \
+        -> Tuple[Callable[[np.ndarray], np.ndarray], List[Any]]:
+    nodes_order = list(nx.topological_sort(causal_model.graph))
+
+    node_samples, noise_samples = \
+        noise_samples_of_ancestors(causal_model, target_node, num_training_samples)
+
+    approx_prediction_model.fit(shape_into_2d(noise_samples[nodes_order].to_numpy()),
+                                shape_into_2d(node_samples[target_node].to_numpy()))
+
+    return approx_prediction_model.predict, nodes_order
+
+
+def noise_samples_of_ancestors(causal_model: StructuralCausalModel,
+                               target_node: Any,
+                               num_samples: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    sorted_nodes = nx.topological_sort(causal_model.graph)
+    all_ancestors_of_node = nx.ancestors(causal_model.graph, target_node)
+    all_ancestors_of_node.update({target_node})
+
+    drawn_noise_samples = {}
+    drawn_samples = {}
+
+    for node in sorted_nodes:
+        if node not in all_ancestors_of_node:
+            continue
+
+        if is_root_node(causal_model.graph, node):
+            noise = causal_model.causal_mechanism(node).draw_samples(num_samples)
+            drawn_noise_samples[node] = noise
+            drawn_samples[node] = noise
+        else:
+            noise = causal_model.causal_mechanism(node).draw_noise_samples(num_samples)
+            drawn_noise_samples[node] = noise
+            drawn_samples[node] = causal_model.causal_mechanism(node).evaluate(
+                column_stack_selected_numpy_arrays(drawn_samples, get_ordered_predecessors(causal_model.graph, node)),
+                noise)
+        if node == target_node:
+            break
+
+    return convert_to_data_frame(drawn_samples), convert_to_data_frame(drawn_noise_samples)
--- dowhy/gcm/stats.py
+++ dowhy/gcm/stats.py
@@ -2,9 +2,12 @@
 future.
 """
 
-from typing import Union, List, Optional
+from typing import Union, List, Optional, Callable
 
 import numpy as np
+from numpy.matlib import repmat
+
+from dowhy.gcm.util.general import shape_into_2d
 
 
 def quantile_based_fwer(p_values: Union[np.ndarray, List[float]],
@@ -43,3 +46,118 @@ def quantile_based_fwer(p_values: Union[np.ndarray, List[float]],
         return float(p_values[0])
     else:
         return float(min(1.0, np.quantile(p_values / quantile, quantile)))
+
+
+def marginal_expectation(prediction_method: Callable[[np.ndarray], np.ndarray],
+                         feature_samples: np.ndarray,
+                         samples_of_interest: np.ndarray,
+                         features_of_interest_indices: List[int],
+                         return_averaged_results: bool = True,
+                         feature_perturbation: str = 'randomize_columns_jointly',
+                         max_batch_size: int = -1) -> np.ndarray:
+    """ Estimates the marginal expectation for samples in samples_of_interest when randomizing features that are not
+    part of features_of_interest_indices. This is, this function estimates
+        y^i = E[Y | do(x^i_s)] := \\int_x_s' E[Y | x^i_s, x_s'] p(x_s') d x_s',
+    where x^i_s is the i-th sample from samples_of_interest, s denotes the features_of_interest_indices and
+    x_s' ~ X_s' denotes the randomized features that are not in s. For an approximation of the integral, the given
+    prediction_method is evaluated multiple times for the same x^i_s, but different x_s' ~ X_s'.
+
+    :param prediction_method: Prediction method of interest. This should expect a numpy array as input for making
+    predictions.
+    :param feature_samples: Samples from the joint distribution. These are used as 'background samples'.
+    :param samples_of_interest: Samples for which the marginal expectation should be estimated.
+    :param features_of_interest_indices: Column indices of the features in s.
+    :param return_averaged_results: If set to True, the expectation over all evaluated samples for the i-th
+    sample_of_interest is returned. If set to False, all corresponding results for the i-th sample are returned.
+    :param feature_perturbation: Type of feature permutation:
+        'randomize_columns_independently': Each feature not in s is randomly permuted separately.
+        'randomize_columns_jointly': All features not in s are jointly permuted. Note that this still represents an
+        interventional distribution.
+    :param max_batch_size: Maximum batch size for a estimating the predictions. This has a significant influence on the
+    overall memory usage. If set to -1, all samples are used in one batch.
+    :return: If return_averaged_results is False, a numpy array where the i-th entry belongs to the marginal expectation
+    of x^i_s when randomizing the remaining features.
+    If return_averaged_results is True, a two dimensional numpy array where the i-th entry contains all
+    predictions for x^i_s when randomizing the remaining features.
+    """
+    feature_samples, samples_of_interest = shape_into_2d(feature_samples, samples_of_interest)
+
+    batch_size = samples_of_interest.shape[0] if max_batch_size == -1 else max_batch_size
+    result = [np.nan] * samples_of_interest.shape[0]
+
+    # Make copy to avoid manipulating the original matrix.
+    feature_samples = np.array(feature_samples)
+
+    features_to_randomize = np.delete(np.arange(0, feature_samples.shape[1]), features_of_interest_indices)
+
+    if feature_perturbation == 'randomize_columns_independently':
+        feature_samples = permute_features(feature_samples, features_to_randomize, False)
+    elif feature_perturbation == 'randomize_columns_jointly':
+        feature_samples = permute_features(feature_samples, features_to_randomize, True)
+    else:
+        raise ValueError("Unknown argument %s as feature_perturbation type!" % feature_perturbation)
+
+    # The given prediction method has to be evaluated multiple times on a large amount of different inputs. Typically,
+    # the batch evaluation of a prediction model on multiple inputs at the same time is significantly faster
+    # than evaluating it on single simples in a for-loop. To make use of this, we try to evaluate as many samples as
+    # possible in one batch call of the prediction method. However, this also requires a lot of memory for many samples.
+    # To overcome potential memory issues, multiple batch calls are performed, each with at most batch_size many
+    # samples. The number of samples that are evaluated is normally
+    # samples_of_interest.shape[0] * feature_samples.shape[0]. Here, we reduce it to
+    # batch_size * feature_samples.shape[0]. If the batch_size would be set 1, then each samples_of_interest is
+    # evaluated one by one in a for-loop.
+    inputs = repmat(feature_samples, batch_size, 1)
+    for offset in range(0, samples_of_interest.shape[0], batch_size):
+        # Each batch consist of at most batch_size * feature_samples.shape[0] many samples. If there are multiple
+        # batches, the offset indicates the index of the current samples_of_interest that has not been evaluated yet.
+        if offset + batch_size > samples_of_interest.shape[0]:
+            # If the batch size would be larger than the remaining amount of samples, it is reduced to only include the
+            # remaining samples_of_interest.
+            adjusted_batch_size = samples_of_interest.shape[0] - offset
+            inputs = inputs[:adjusted_batch_size * feature_samples.shape[0]]
+        else:
+            adjusted_batch_size = batch_size
+
+        for index in range(adjusted_batch_size):
+            # The inputs consist of batch_size many copies of feature_samples. Here, we set the columns of the features
+            # in features_of_interest_indices to their respective values in samples_of_interest.
+            inputs[index * feature_samples.shape[0]:(index + 1) * feature_samples.shape[0],
+            features_of_interest_indices] = samples_of_interest[offset + index, features_of_interest_indices]
+
+        # After creating the (potentially large) input data matrix, we can evaluate the prediction method.
+        predictions = np.array(prediction_method(inputs))
+
+        for index in range(adjusted_batch_size):
+            # Here, offset + index now indicates the sample index in samples_of_interest.
+            if return_averaged_results:
+                # This would average all prediction results obtained for the 'offset + index'-th sample in
+                # samples_of_interest. This is, y^(offset + index) = E[Y | do(x^(offset + index)_s)].
+                result[offset + index] = np.mean(predictions[index * feature_samples.shape[0]:
+                                                             (index + 1) * feature_samples.shape[0]], axis=0)
+            else:
+                # This would return all prediction results obtained for the 'offset + index'-th sample in
+                # samples_of_interest, i.e. the results are not averaged.
+                result[offset + index] = predictions[index * feature_samples.shape[0]:
+                                                     (index + 1) * feature_samples.shape[0]]
+
+    return np.array(result)
+
+
+def permute_features(feature_samples: np.ndarray,
+                     features_to_permute: Union[List[int], np.ndarray],
+                     randomize_features_jointly: bool) -> np.ndarray:
+    # Making copy to ensure that the original object is not modified.
+    feature_samples = np.array(feature_samples)
+
+    if randomize_features_jointly:
+        # Permute samples jointly. This still represents an interventional distribution.
+        feature_samples[:, features_to_permute] \
+            = feature_samples[np.random.choice(feature_samples.shape[0],
+                                               feature_samples.shape[0],
+                                               replace=False)][:, features_to_permute]
+    else:
+        # Permute samples independently.
+        for feature in features_to_permute:
+            np.random.shuffle(feature_samples[:, feature])
+
+    return feature_samples
