51b3f92543b90db51dc2322360bb9baae29049b7
michaelmarien <marien.mich@gmail.com>
2022-07-02 10:53:15 +0200

    add test to make sure bootstrap_test is returning two sided p values for refutation

--- tests/test_causal_refuter.py
+++ tests/test_causal_refuter.py
@@ -1,7 +1,10 @@
 import pytest
+from flaky import flaky
+import numpy as np
 
 from dowhy.causal_refuter import CausalRefuter
 from dowhy.causal_identifier import IdentifiedEstimand
+from dowhy.causal_estimator import CausalEstimate
 
 
 class MockRefuter(CausalRefuter):
@@ -12,3 +15,12 @@ def test_causal_refuter_placeholder_method():
 	refuter = MockRefuter(None, IdentifiedEstimand(None, None, None), None)
 	with pytest.raises(NotImplementedError):
 		refuter.refute_estimate()
+
+
+@flaky(max_runs=3)
+def test_causal_refuter_bootstrap_test():
+	estimator = CausalEstimate(0, None, None, None, None)
+	refuter = MockRefuter(None, IdentifiedEstimand(None, None, None), None)
+	simulations = np.random.normal(0, 1, 5000)
+	pvalue = refuter.perform_bootstrap_test(estimator, simulations)
+	assert pvalue > .95
