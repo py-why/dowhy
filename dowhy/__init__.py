a87ecc44b7fd634e7f49b465d32b6b62f6fc8853
Chris Trevino <chtrevin@microsoft.com>
2022-07-29 14:36:51 -0700

    remove import from _version
    Signed-off-by: Chris Trevino <chtrevin@microsoft.com>

--- dowhy/__init__.py
+++ dowhy/__init__.py
@@ -1,8 +1,10 @@
 import logging
 from dowhy.causal_model import CausalModel
-from . import _version
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
+#
 # 0.0.0 is standard placeholder for poetry-dynamic-versioning
+# any changes to this should not be checked in
+#
 __version__ = "0.0.0"
