c3b9c8a91ce7a44e644d21080846a46a5593fe47
Chris Trevino <chtrevin@microsoft.com>
2022-07-29 09:41:52 -0700

    use poetry-dynamic-versioning
    Signed-off-by: Chris Trevino <chtrevin@microsoft.com>

--- dowhy/__init__.py
+++ dowhy/__init__.py
@@ -1,10 +1,10 @@
 import logging
 from os import path
 from dowhy.causal_model import CausalModel
-
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 here = path.abspath(path.dirname(__file__))
-# Loading version number
-with open(path.join(here, 'VERSION')) as version_file:
-    __version__ = version_file.read().strip()
+
+
+# 0.0.0 is standard placeholder for poetry-dynamic-versioning
+__version__ = "0.0.0"
