fixed the computation errors
Signed-off-by: Amit Sharma <amit_sharma@live.com>

dowhy/causal_refuters/non_parametric_sensitivity_analyzer.py
@@ -5,21 +5,22 @@ import logging
 import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
 from econml.utilities import cross_product
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer, make_column_selector
-from dowhy.utils.util import get_numeric_features
+from dowhy.causal_refuters.reisz import get_alpha_estimator
+from dowhy.utils.util import get_numeric_features, generate_moment_function, get_generic_regressor
 from dowhy.causal_refuters.partial_linear_sensitivity_analyzer import PartialLinearSensitivityAnalyzer
-from dowhy.causal_refuters.reisz import ReiszRegressor, ReiszRepresenter, generate_moment_function, get_alpha_estimator, get_generic_regressor, create_polynomial_function
 
 
 class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
     """
     Non-parametric sensitivity analysis for causal estimators. 
 
-    Currently works only for single-dimensional treatment. 
+    Currently works only for single-dimensional treatment and binary treatment because it uses a plugin estimate of reisz function. 
+   
+    TODO: Extend to general treatments by implementing the loss-based reisz representer.  
 
     Two important quantities used to estimate the bias are alpha and g.
          g := E[Y | T, W, Z] denotes the long regression function
@@ -43,19 +44,21 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
     :param significance_level: confidence interval for statistical inference(default = 0.05)
     :param frac_strength_treatment: strength of association between unobserved confounder and treatment compared to benchmark covariate
     :param frac_strength_outcome: strength of association between unobserved confounder and outcome compared to benchmark covariate
-    :param alpha_s_param_dict: dictionary with parameters for finding alpha_s 
     :param g_s_estimator_list: list of estimator objects for finding g_s. These objects should have fit() and predict() functions.
     :param g_s_estimator_param_list: list of dictionaries with parameters for tuning respective estimators in "g_s_estimator_list". 
+    :param alpha_s_estimator_list: list of estimator objects for finding the treatment predictor which is used for alpha_s estimation. These objects should have fit() and predict_proba() functions.
+    :param alpha_s_estimator_param_list: list of dictionaries with parameters for tuning respective estimators in "alpha_s_estimator_list". 
                                          The order of the dictionaries in the list should be consistent with the estimator objects order in "g_s_estimator_list"
     :param observed_common_causes: common causes dataframe
     :param outcome: outcome dataframe
     :param treatment: treatment dataframe
     :param theta_s: point estimate for the estimator
+    :param plugin_reisz: whether to use plugin reisz estimator. False by default. 
     
     """
 
     def __init__(self, *args, theta_s, 
-            max_frac_inc_reisz = 0.99, **kwargs):
+            max_frac_inc_reisz = 0.99, plugin_reisz=False, **kwargs):
         super().__init__(*args, **kwargs)
         self.theta_s = theta_s
         self.max_frac_inc_reisz = max_frac_inc_reisz
@@ -68,6 +71,7 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
         self.m_alpha = None
         self.g_s = None
         self.m_g = None
+        self.plugin_reisz = plugin_reisz
 
     def check_sensitivity(self, plot=True):
         """
@@ -89,10 +93,11 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
 
         # features are the observed confounders
         features = self.observed_common_causes.copy()
-        treatment_df = self.treatment.copy()
+        treatment_df = self.treatment.copy().astype(int)
         W = features.to_numpy()
         X = pd.concat([treatment_df, features], axis=1)
         numeric_features = get_numeric_features(X = X)
+        numeric_features_alpha = get_numeric_features(X=features)
         T = treatment_df.values.ravel()
 
         X = X.to_numpy()
@@ -113,6 +118,7 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
         self.m_g = np.zeros(num_samples)
         self.g_s_j = np.zeros(num_samples)
         self.alpha_s_j = np.zeros(num_samples)
+        propensities = np.zeros(num_samples)
 
         reg_function = get_generic_regressor(cv=split_indices,
                                              X=X, Y=Y, max_degree=self.reisz_polynomial_max_degree,
@@ -120,22 +126,28 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
                                              estimator_param_list=self.g_s_estimator_param_list,
                                              numeric_features=numeric_features
                                              )
-
         reisz_function = get_alpha_estimator(
-            cv=split_indices, X=X, max_degree=self.reisz_polynomial_max_degree, param_grid_dict=self.alpha_s_param_dict)
-
+            cv=split_indices, X=X, 
+            max_degree=self.reisz_polynomial_max_degree, 
+            estimator_list=self.alpha_s_estimator_list,
+            estimator_param_list=self.alpha_s_estimator_param_list,
+            numeric_features=numeric_features_alpha,
+            plugin_reisz=self.plugin_reisz)
         for train, test in split_indices:
             reisz_fn_fit = reisz_function.fit(X[train])
             self.alpha_s[test] = reisz_fn_fit.predict(X[test])
+            propensities[test] = reisz_fn_fit.propensity(X[test])
             self.m_alpha[test] = self.moment_function(
-                X[test], reisz_fn_fit.predict)
+                    X[test], reisz_fn_fit.predict)
 
             reg_fn_fit = reg_function.fit(X[train], Y[train])
             self.g_s[test] = reg_fn_fit.predict(X[test])
             self.m_g[test] = self.moment_function(X[test], reg_fn_fit.predict)
-
         self.m = self.m_g + self.alpha_s * (Y - self.g_s)
-
+        if self.plugin_reisz:
+            self.var_alpha_s = np.mean(1 / (propensities * (1-propensities)))
+        else:
+            self.var_alpha_s = np.var(self.alpha_s)
         self.nu_2 = np.mean(
             2 * self.m_alpha[indices] - self.alpha_s[indices] ** 2)
         self.sigma_2 = np.mean((Y[indices] - self.g_s[indices]) ** 2)
@@ -153,8 +165,7 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
         self.r2y_tw = np.var(self.g_s) / np.var(Y)
 
         # R^2 of treatment with observed common causes
-        numeric_features = get_numeric_features(features)
-        self.r2t_w = self.get_regression_r2(X = W, Y = T, numeric_features = numeric_features, split_indices = split_indices)
+        self.r2t_w = self.get_regression_r2(X = W, Y = T, numeric_features = numeric_features_alpha, split_indices = split_indices)
 
         delta_r2_y_wj, var_alpha_wj = self.compute_r2diff_benchmarking_covariates(
             treatment_df, features, T, Y, W, self.benchmark_common_causes,
@@ -163,7 +174,7 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
         # Partial R^2 of outcome after regressing over unobserved confounder, observed common causes and treatment
         delta_r2y_u = self.frac_strength_outcome * delta_r2_y_wj
         # The difference in R2 is the same for wj and new unobserved confounder 
-        c2_alpha = self.frac_strength_treatment * (np.var(self.alpha_s)/var_alpha_wj - 1)#self.frac_strength_treatment * delta_r2t_wj
+        c2_alpha = self.frac_strength_treatment * (self.var_alpha_s/var_alpha_wj - 1)#self.frac_strength_treatment * delta_r2t_wj
 
         self.r2yu_tw = abs(delta_r2y_u / (1 - self.r2y_tw))
         self.r2tu_w = c2_alpha
@@ -176,7 +187,6 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
             significance_level=self.significance_level,
             is_partial_linear=self.is_partial_linear)
         self.results = pd.DataFrame(benchmarking_results, index=[0])
-
         self.RV = self.calculate_robustness_value(alpha=None, is_partial_linear=self.is_partial_linear)
         self.RV_alpha = self.calculate_robustness_value(
             alpha=self.significance_level,
@@ -203,29 +213,35 @@ class NonParametricSensitivityAnalyzer(PartialLinearSensitivityAnalyzer):
 
         return bounds_estimator_lower, bounds_estimator_upper
 
-    def get_alpharegression_var(self, X, split_indices,
+    def get_alpharegression_var(self, X, numeric_features, split_indices,
             reisz_model=None):
         """
-        Calculates the pearson non parametric partial R^2 from a regression function.
+        Calculates the variance of reisz function
 
         :param X: numpy array containing set of regressors 
-        :param numeric_features: list of indices of columns with numeric features
         :param split_indices: training and testing data indices obtained after cross folding
 
-        :returns: partial R^2 value
+        :returns: variance of reisz function
         """
         if reisz_model is None:
             reisz_model = get_alpha_estimator(
                 cv=split_indices, X=X, 
                 max_degree=self.reisz_polynomial_max_degree, 
-                param_grid_dict=self.alpha_s_param_dict)
+                estimator_list=self.alpha_s_estimator_list,
+                estimator_param_list=self.alpha_s_estimator_param_list,
+                numeric_features=numeric_features, 
+                plugin_reisz=self.plugin_reisz)
 
         num_samples = X.shape[0]
         pred = np.zeros(num_samples) 
-        for train, test in split_indices:
-            reisz_fn_fit = reisz_model.fit(X[train])
-            pred[test] = reisz_fn_fit.predict(X[test])
-
-        return np.var(pred)
-
-
+        if self.plugin_reisz:
+            for train, test in split_indices:
+                reisz_fn_fit = reisz_model.fit(X[train])
+                pred[test] = reisz_fn_fit.propensity(X[test])
+            newpred = 1 / (pred * (1-pred))
+            return np.mean(newpred)
+        else:
+            for train, test in split_indices:
+                reisz_fn_fit = reisz_model.fit(X[train])
+                pred[test] = reisz_fn_fit.predict(X[test])
+            return np.var(pred)

dowhy/causal_refuters/reisz.py
@@ -4,12 +4,105 @@ from econml.grf._base_grf import BaseGRF
 from econml.utilities import cross_product
 from econml.sklearn_extensions.model_selection import GridSearchCVList
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.compose import ColumnTransformer, make_column_selector
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.compose import ColumnTransformer, make_column_selector
-from dowhy.utils.util import get_numeric_features
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from dowhy.utils.util import generate_moment_function, create_polynomial_function, get_generic_regressor
 
+def get_alpha_estimator(cv, X, max_degree = None, estimator_list = None, estimator_param_list = None, numeric_features = None, plugin_reisz=True):
+    """
+    Finds the best estimator for reisz representer (alpha_s )
+
+    :param cv: training and testing data indices obtained afteer Kfolding the dataset
+    :param X: treatment+confounders
+    :param max_degree: degree of the polynomial function used to approximate alpha_s
+    :param param_grid_dict: python dictionary with parameters to tune the ReiszRepresenter estimator
+
+    :returns: estimator for alpha_s
+
+    This method assumes a binary T. 
+    """
+
+    if plugin_reisz:
+        if estimator_param_list is None:
+            estimator_list = [
+                RandomForestClassifier(n_estimators=100, random_state=120),
+                Pipeline([('scale', ColumnTransformer([('num', StandardScaler(), numeric_features)],
+                                                      remainder='passthrough')),
+                          ('logistic_model', LogisticRegression())]),
+                GradientBoostingClassifier()
+                ]
+            estimator_param_list = [
+                    {'n_estimators': [50],
+                     'max_depth': [3, 4, 5],
+                     'min_samples_leaf': [10, 50]
+                     },
+                    {'logistic_model__C': [1, 0.01, 0.001, 1e-4, 1e-5]},
+                    {
+                         'learning_rate' : [0.01, 0.001],
+                         'n_estimators' : [50,200]
+                    }
+                ]
+        W = X[:, 1:] # assume 1-D treatment
+        t = X[:, 0]
+        propmodel = get_generic_regressor(cv=cv, X=W, Y=t,
+                max_degree = max_degree, estimator_list = estimator_list,
+                estimator_param_list = estimator_param_list, numeric_features = numeric_features)
+        reisz_fn = PluginReisz(propmodel)
+    else:
+        if estimator_param_list is None:
+            estimator_param_list = [{'reisz_functions': [create_polynomial_function(2), create_polynomial_function(1)],
+                                    'min_samples_leaf': [10, 50],
+                                    'min_var_fraction_leaf': [0.01, 0.1],
+                                    'l2_regularizer': [1e-5, 1e-3],
+                                    'max_depth': [5, None]}]
+        estimator = GridSearchCVList([
+                ReiszRepresenter(reisz_functions=create_polynomial_function(max_degree),
+                             moment_function=generate_moment_function,
+                             min_samples_leaf=10,
+                             min_var_fraction_leaf=0.01,
+                             max_depth=5)
+                ],
+                param_grid_list=estimator_param_list,
+                scoring=reisz_scoring,
+                cv=cv,
+                verbose=0,
+                n_jobs=-1
+            ).fit(X)
+        reisz_fn = (estimator.best_estimator_)
+    return reisz_fn
+
+
+class PluginReisz:
+    """Plugin reisz function for average treatment effect
+
+    """
+
+    def __init__(self, propensity_model):
+        self.propmodel = propensity_model
+
+    def fit(self, X):
+        W = X[:, 1:] # assume 1-D treatment
+        t = X[:, 0]
+        self.propmodel.fit(W,t)
+        return self
+
+    def predict(self, X):
+        W = X[:, 1:] # assume 1-D treatment
+        t = X[:, 0]
+        preds = self.propmodel.predict_proba(W)
+        weights = [1/preds[i,t[i].astype(int)] for i in range(preds.shape[0])]
+        weights = np.where(t==0, -1, 1) * weights 
+        return weights
+
+    def propensity(self, X):
+        """
+        P(T=1|W)
+        """
+        W = X[:, 1:] # assume 1-D treatment
+        return self.propmodel.predict_proba(W)[:,1]
 
 def reisz_scoring(g, X):
     """
@@ -23,134 +116,6 @@ def reisz_scoring(g, X):
     loss = np.mean(2 * generate_moment_function(W=X,
                    g=g.predict) - g.predict(X) ** 2)
     return loss
-
-
-def get_alpha_estimator(cv, X, max_degree, param_grid_dict):
-    """
-    Finds the best estimator for reisz representer (alpha_s )
-
-    :param cv: training and testing data indices obtained afteer Kfolding the dataset
-    :param X: training data for the model
-    :param max_degree: degree of the polynomial function used to approximate alpha_s
-    :param param_grid_dict: python dictionary with parameters to tune the ReiszRepresenter estimator
-
-    :returns: estimator for alpha_s
-    """
-    if param_grid_dict is not None:
-        estimator = GridSearchCVList([
-            ReiszRepresenter(reisz_functions=create_polynomial_function(max_degree),
-                             moment_function=generate_moment_function,
-                             min_samples_leaf=10,
-                             min_var_fraction_leaf=0.01,
-                             max_depth=5)
-        ],
-            param_grid_list=[param_grid_dict],
-            scoring=reisz_scoring,
-            cv=cv,
-            verbose=0,
-            n_jobs=-1
-        ).fit(X)
-        return (estimator.best_estimator_)
-    else:
-        estimator = ReiszRepresenter(reisz_functions=create_polynomial_function(max_degree),
-                                     moment_function=generate_moment_function,
-                                     min_samples_leaf=10,
-                                     min_var_fraction_leaf=0.01,
-                                     max_depth=5).fit(X)
-        return estimator
-
-
-def get_generic_regressor(cv, X, Y, max_degree = 3, estimator_list = None, estimator_param_list = None, numeric_features = None):
-    """
-    Finds the best estimator for reisz regression function (g_s)
-
-    :param cv: training and testing data indices obtained afteer Kfolding the dataset
-    :param X: regressors data for training the regression model
-    :param Y: outcome data for training the regression model
-    :param max_degree: degree of the polynomial function used to approximate the regression function
-    :param estimator_list: list of estimator objects for finding the regression function
-    :param estimator_param_list: list of dictionaries with parameters for tuning respective estimators in estimator_list
-    :param numeric_features: list of indices of numeric features in the dataset
-
-    :returns: estimator for Reisz Regression function
-    """
-    if estimator_list is not None:
-        estimator = GridSearchCVList(estimator_list,
-                                     estimator_param_list,
-                                     cv=cv,
-                                     scoring='neg_mean_squared_error',
-                                     n_jobs=-1
-                                     ).fit(X, Y)
-        return estimator.best_estimator_
-    else:
-        estimator = GridSearchCVList([
-            RandomForestRegressor(n_estimators=100, random_state=120),
-
-            Pipeline([('scale', ColumnTransformer([('num', StandardScaler(), numeric_features)],
-                                                  remainder='passthrough')),
-                      ('lasso_model', Lasso())]),
-
-            ReiszRegressor(regression_functions=create_polynomial_function(max_degree),
-                           min_var_leaf_on_val=True,
-                           min_impurity_decrease=1e-4,
-                           max_samples=0.80,
-                           inference=False,
-                           subforest_size=1,
-                           random_state=123),
-            GradientBoostingRegressor()
-        ],
-            param_grid_list=[
-                {'n_estimators': [50],
-                 'max_depth': [3, 4, 5],
-                 'min_samples_leaf': [10, 50]
-                 },
-
-                {'lasso_model__alpha': [0.01, 0.001, 1e-4, 1e-5, 1e-6]},
-
-                {'regression_functions': [create_polynomial_function(2), create_polynomial_function(4)],
-                 'min_samples_leaf': [10, 50],
-                 'min_var_fraction_leaf': [0.01, 0.1],
-                 'l2_regularizer': [1e-2, 1e-3]
-                 },
-                 {
-                     'learning_rate' : [0.01, 0.001],
-                     'n_estimators' : [50,200]
-                 }
-        ],
-            cv=cv,
-            scoring='neg_mean_squared_error',
-            n_jobs=-1
-        ).fit(X, Y)
-        return estimator.best_estimator_
-
-
-def generate_moment_function(W, g):
-    """
-    Generate and returns moment function
-    m(W,g) = g(1,W) - g(0,W) for Average Causal Effect
-    """
-    shape = (W.shape[0], 1)
-    ones = np.ones(shape)
-    zeros = np.zeros(shape)
-    non_treatment_data = W[:, 1:] # assume that treatment is one-dimensional. 
-    data_0 = np.hstack([zeros, non_treatment_data])  # data with treatment = 1
-    data_1 = np.hstack([ones, non_treatment_data])  # data with treatment = 0
-    return g(data_1) - g(data_0)
-
-
-def create_polynomial_function(max_degree):
-    """
-    Creates a list of polynomial functions
-
-    :param max_degree: degree of the polynomial function to be created
-
-    :returns: list of lambda functions
-    """
-    polynomial_function = []
-    for degree in range(max_degree+1):
-        def poly_term(x): return x[:, [0]] ** degree
-        polynomial_function.append(poly_term)
-    return polynomial_function
 
 
 class ReiszRepresenter(BaseGRF):
@@ -249,94 +214,3 @@ class ReiszRepresenter(BaseGRF):
         point = super().predict(X_test[:, 1:])
         return self._translate(point, X_test)
 
-
-class ReiszRegressor(BaseGRF):
-    """
-    A forest to estimate reisz regression function
-
-    :param regression_functions: List of polynomial functions of n degree to approximate regression function created using create_polynomial_function
-    :param l2_regularizer: l2 penalty while modeling (default = 1e-3)
-    For tuning other parameters see https://econml.azurewebsites.net/_autosummary/econml.grf.CausalForest.html 
-
-    """
-
-    def __init__(self, *,
-                 regression_functions,
-                 l2_regularizer=1e-3,
-                 n_estimators=100,
-                 criterion="mse",
-                 max_depth=None,
-                 min_samples_split=10,
-                 min_samples_leaf=5,
-                 min_weight_fraction_leaf=0.,
-                 min_var_fraction_leaf=None,
-                 min_var_leaf_on_val=False,
-                 max_features="auto",
-                 min_impurity_decrease=0.,
-                 max_samples=.45,
-                 min_balancedness_tol=.45,
-                 honest=True,
-                 inference=True,
-                 fit_intercept=True,
-                 subforest_size=4,
-                 n_jobs=-1,
-                 random_state=None,
-                 verbose=0,
-                 warm_start=False):
-
-        self.regression_functions = regression_functions
-        self.num_reg_functions = len(self.regression_functions)
-        self.l2_regularizer = l2_regularizer
-
-        super().__init__(n_estimators=n_estimators,
-                         criterion=criterion,
-                         max_depth=max_depth,
-                         min_samples_split=min_samples_split,
-                         min_samples_leaf=min_samples_leaf,
-                         min_weight_fraction_leaf=min_weight_fraction_leaf,
-                         min_var_fraction_leaf=min_var_fraction_leaf,
-                         min_var_leaf_on_val=min_var_leaf_on_val,
-                         max_features=max_features,
-                         min_impurity_decrease=min_impurity_decrease,
-                         max_samples=max_samples,
-                         min_balancedness_tol=min_balancedness_tol,
-                         honest=honest,
-                         inference=inference,
-                         fit_intercept=False,
-                         subforest_size=subforest_size,
-                         n_jobs=n_jobs,
-                         random_state=random_state,
-                         verbose=verbose,
-                         warm_start=warm_start)
-
-    def fit(self, X, y):
-        return super().fit(X[:, 1:], X[:, [0]], y)
-
-    def _get_alpha_and_pointJ(self, X, T, y):
-        n_reg_feats = len(self.regression_functions)
-        n_feats = n_reg_feats
-        TX = np.hstack([T, X])
-        reg_feats = np.hstack([feat_fn(TX)
-                               for feat_fn in self.regression_functions])
-        alpha = y.reshape(-1, 1) * reg_feats
-        reg_cov_matrix = cross_product(reg_feats, reg_feats).reshape(
-            (X.shape[0], n_reg_feats, n_reg_feats))
-        penalty = self.l2_regularizer * np.eye(n_reg_feats)
-        penalty[0, 0] = 0
-        pointJ = reg_cov_matrix + penalty
-        return alpha, pointJ.reshape((X.shape[0], -1))
-
-    def _get_n_outputs_decomposition(self, X, T, y):
-        n_relevant_outputs = len(self.regression_functions)
-        n_outputs = n_relevant_outputs
-        return n_outputs, n_relevant_outputs
-
-    def _translate(self, point, TX):
-        reg_feats = np.hstack([feat_fn(TX)
-                               for feat_fn in self.regression_functions])
-        reg = np.sum(point * reg_feats, axis=1)
-        return reg
-
-    def predict(self, X_test):
-        point = super().predict(X_test[:, 1:])
-        return self._translate(point, X_test)