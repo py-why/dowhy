05bae850a62900ea920bacc1c35901945be16909
Chris Trevino <darthtrevino@gmail.com>
2022-10-28 15:48:06 -0700

    restore gcm notebook tests
    Signed-off-by: Chris Trevino <darthtrevino@gmail.com>

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -28,10 +28,7 @@ advanced_notebooks = [
     "dowhy-conditional-treatment-effects.ipynb",
     "dowhy_refuter_notebook.ipynb",
     "dowhy_twins_example.ipynb",
-    "gcm_rca_microservice_architecture.ipynb",
-    "gcm_supply_chain_dist_change.ipynb",
     "dowhy_simple_example.ipynb",
-    "gcm_401k_analysis.ipynb",
 ]
 
 ignore_notebooks = [
