fc7855dc4f061c8e03b0a517e7d6de79571b8e58
Chris Trevino <darthtrevino@gmail.com>
2022-11-07 14:28:45 -0800

    improve clarity in the generate_docs script
    Signed-off-by: Chris Trevino <darthtrevino@gmail.com>

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -4,19 +4,27 @@ cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 OUTPUT_DIR='../dowhy-docs'
 
-mv source/conf.py source/conf.py.orig
+#
+# Build <0.9 Versions using RTD Theme
+#
 cp source/conf-rtd.py source/conf.py
 cp source/_templates/versions-rtd.html source/_templates/versions.html
 
 poetry run sphinx-multiversion --dump-metadata source ${OUTPUT_DIR}
 poetry run sphinx-multiversion source ${OUTPUT_DIR}
 
-mv source/conf.py.orig source/conf.py
+#
+# Build >= 0.9 and main branch using Pydata THeme
+#
+mv source/conf-pydata.py source/conf.py
 cp source/_templates/versions-pydata.html source/_templates/versions.html
 
 poetry run sphinx-multiversion --dump-metadata source ${OUTPUT_DIR}
 poetry run sphinx-multiversion source ${OUTPUT_DIR}
 
+#
+# Create the top-level index.html
+#
 STABLE_VERSION=$(git describe --tags --abbrev=0 --match='v*')
 
 echo "<html>
