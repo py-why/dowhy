f08a2edb7cf6d77cb4d80ffcaa24ca13d446019f
Chris Trevino <darthtrevino@gmail.com>
2022-11-07 15:55:01 -0800

    formatting
    Signed-off-by: Chris Trevino <darthtrevino@gmail.com>

--- docs/source/conf-pydata.py
+++ docs/source/conf-pydata.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+
 from docs.source.version_filter import create_version_filter
 
 sys.path.insert(0, os.path.abspath("../../"))
--- docs/source/version_filter.py
+++ docs/source/version_filter.py
@@ -1,11 +1,12 @@
 import os
 
-def create_version_filter(include: str): 
-  # Create a negative-lookahead regexp to exclude the version from being generated
-  def exclude(dir): 
-    return "(?!" + dir.replace('.', '\.') + ")"
-  
-  versions = os.listdir('../../dowhy-docs')
-  versions.remove('index.html')
-  versions.remove('.nojekyll')
-  return "^".join(list(map(exclude, versions))) + include + "$"
+
+def create_version_filter(include: str):
+    # Create a negative-lookahead regexp to exclude the version from being generated
+    def exclude(dir):
+        return "(?!" + dir.replace(".", "\.") + ")"
+
+    versions = os.listdir("../../dowhy-docs")
+    versions.remove("index.html")
+    versions.remove(".nojekyll")
+    return "^".join(list(map(exclude, versions))) + include + "$"
