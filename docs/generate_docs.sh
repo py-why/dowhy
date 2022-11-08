a033160ad02118ed689c5cfa76bff34894102726
Chris Trevino <darthtrevino@gmail.com>
2022-11-08 15:35:45 -0800

    add argument for target docs version
    Signed-off-by: Chris Trevino <darthtrevino@gmail.com>

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DOCS_ROOT='../dowhy-docs'
-OUTPUT_DIR="${DOCS_ROOT}/main"
+# To change the build target, specify the DOCS_VERSION environment variable. (e.g. DOCS_VERSION=v0.8)
+TARGET_BUILD=${DOCS_VERSION:-main}
+OUTPUT_DIR="${DOCS_ROOT}/${TARGET_BUILD}"
 STABLE_VERSION=$(git describe --tags --abbrev=0 --match='v*')
+
+echo "Building docs for version ${TARGET_BUILD} into ${OUTPUT_DIR}, stable version is ${STABLE_VERSION}"
 
 #
 # Cache existing docs
