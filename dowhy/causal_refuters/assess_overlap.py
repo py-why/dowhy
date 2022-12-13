5fbeb4329bf5a5e9a6f070c98c4f9d1180bf4913
Michael Oberst <michael.k.oberst@gmail.com>
2022-12-12 19:26:03 -0500

    feat: Pass in additional arguments to OverRule
    Signed-off-by: Michael Oberst <michael.k.oberst@gmail.com>

--- dowhy/causal_refuters/assess_overlap.py
+++ dowhy/causal_refuters/assess_overlap.py
@@ -24,10 +24,11 @@ class AssessOverlap(CausalRefuter):
         """
         super().__init__(*args, **kwargs)
         self._backdoor_vars = self._target_estimand.get_backdoor_variables()
-        if "cat_feats" in kwargs:
-            self._cat_feats = kwargs.get("cat_feats")
-        else:
-            self._cat_feats = []
+        self._cat_feats = kwargs.pop("cat_feats", [])
+        self._support_config = kwargs.pop("support_config", None)
+        self._overlap_config = kwargs.pop("overlap_config", None)
+        self._overlap_eps = kwargs.pop("overlap_eps", 0.1)
+        self._overrule_verbose = kwargs.pop("overrule_verbose", False)
 
     def refute_estimate(self, show_progress_bar=False):
         return assess_support_and_overlap_overrule(
@@ -35,12 +36,24 @@ class AssessOverlap(CausalRefuter):
             backdoor_vars=self._backdoor_vars,
             treatment_name=self._treatment_name,
             cat_feats=self._cat_feats,
+            overlap_config=self._overlap_config,
+            support_config=self._support_config,
+            overlap_eps=self._overlap_eps,
+            verbose=self._overrule_verbose,
         )
 
 
-def assess_support_and_overlap_overrule(data, backdoor_vars, treatment_name, cat_feats: List[str] = []):
+def assess_support_and_overlap_overrule(
+    data, backdoor_vars, treatment_name, cat_feats, overlap_config, support_config, overlap_eps, verbose
+):
     X = data[backdoor_vars]
     t = data[treatment_name]
-    analyzer = OverruleAnalyzer(cat_feats=cat_feats)
+    analyzer = OverruleAnalyzer(
+        cat_feats=cat_feats,
+        overlap_config=overlap_config,
+        support_config=support_config,
+        overlap_eps=overlap_eps,
+        verbose=verbose,
+    )
     analyzer.fit(X, t)
     return analyzer
