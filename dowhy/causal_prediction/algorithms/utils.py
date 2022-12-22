1ac8354e446bc7a9b2278ac1d9872eab5863e8a2
Jivat Neet <39404029+jivatneet@users.noreply.github.com>
2022-12-22 19:21:12 +0530

    Adding causal prediction functionality using CACM algorithm with demo notebook (#729)
    * add causal prediction using cacm + demo notebook
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    
    * Adding causal prediction code using CACM with demo notebook
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    
    * addressing comments: fixing documentation in base_algorithm and cacm
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    
    * addressing comments: fixing documentation in base_dataset
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    
    * rename Algorithm class
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    
    * updating base_dataset for format check
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    
    * adding exception for optimizer; resolving list bug
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    
    * adding docs for base algo
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    
    * added torch to dev dependency
    
    * added torchlightning to dev dependency
    
    Signed-off-by: Amit Sharma <amit_sharma@live.com>
    
    * updated poetry.lock
    
    Signed-off-by: Amit Sharma <amit_sharma@live.com>
    
    * updated gpu to devices
    
    Signed-off-by: Amit Sharma <amit_sharma@live.com>
    
    Signed-off-by: jivatneet <jivatneet@gmail.com>
    Signed-off-by: Amit Sharma <amit_sharma@live.com>
    Co-authored-by: Amit Sharma <amit_sharma@live.com>

---
+++ dowhy/causal_prediction/algorithms/utils.py
@@ -0,0 +1,49 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+""" 
+    The functions in this file are borrowed from DomainBed: https://github.com/facebookresearch/DomainBed
+        @inproceedings{gulrajani2021in,
+         title={In Search of Lost Domain Generalization},
+         author={Ishaan Gulrajani and David Lopez-Paz},
+         booktitle={International Conference on Learning Representations},
+         year={2021},
+        }
+"""
+
+import torch
+
+
+def my_cdist(x1, x2):
+    x1_norm = x1.pow(2).sum(dim=-1, keepdim=True)
+    x2_norm = x2.pow(2).sum(dim=-1, keepdim=True)
+    res = torch.addmm(x2_norm.transpose(-2, -1), x1, x2.transpose(-2, -1), alpha=-2).add_(x1_norm)
+    return res.clamp_min_(1e-30)
+
+
+def gaussian_kernel(x, y, gamma):
+    D = my_cdist(x, y)
+    K = torch.zeros_like(D)
+
+    K.add_(torch.exp(D.mul(-gamma)))
+
+    return K
+
+
+def mmd_compute(x, y, kernel_type, gamma):
+    if kernel_type == "gaussian":
+        Kxx = gaussian_kernel(x, x, gamma).mean()
+        Kyy = gaussian_kernel(y, y, gamma).mean()
+        Kxy = gaussian_kernel(x, y, gamma).mean()
+        return Kxx + Kyy - 2 * Kxy
+    else:
+        mean_x = x.mean(0, keepdim=True)
+        mean_y = y.mean(0, keepdim=True)
+        cent_x = x - mean_x
+        cent_y = y - mean_y
+        cova_x = (cent_x.t() @ cent_x) / (len(x) - 1)
+        cova_y = (cent_y.t() @ cent_y) / (len(y) - 1)
+
+        mean_diff = (mean_x - mean_y).pow(2).mean()
+        cova_diff = (cova_x - cova_y).pow(2).mean()
+
+        return mean_diff + cova_diff
---
+++ dowhy/causal_prediction/datasets/base_dataset.py
@@ -0,0 +1,25 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+""" 
+    `MultipleDomainDataset` class in this file is borrowed from DomainBed: https://github.com/facebookresearch/DomainBed
+        @inproceedings{gulrajani2021in,
+         title={In Search of Lost Domain Generalization},
+         author={Ishaan Gulrajani and David Lopez-Paz},
+         booktitle={International Conference on Learning Representations},
+         year={2021},
+        }
+"""
+
+
+class MultipleDomainDataset:
+    N_STEPS = 5001  # Default, subclasses may override
+    CHECKPOINT_FREQ = 100  # Default, subclasses may override
+    N_WORKERS = 8  # Default, subclasses may override
+    ENVIRONMENTS = None  # Subclasses should override
+    INPUT_SHAPE = None  # Subclasses should override
+
+    def __getitem__(self, index):
+        return self.datasets[index]
+
+    def __len__(self):
+        return len(self.datasets)
