48113b329549171fa53fdb82af4cf22bdce857db
Amit Sharma <amit_sharma@live.com>
2023-12-22 16:11:45 +0530

    formatted correctly
    Signed-off-by: Amit Sharma <amit_sharma@live.com>

--- dowhy/causal_estimator.py
+++ dowhy/causal_estimator.py
@@ -351,8 +351,8 @@ class CausalEstimator:
         sorted_bootstrap_variations = np.sort(bootstrap_variations)
 
         # Now we take the (1-p)/2 th and the 1-(1-p)/2 th variations, where p is the chosen confidence level
-        left_fraction = (1 - confidence_level)/2 
-        right_fraction = 1 - left_fraction 
+        left_fraction = (1 - confidence_level) / 2
+        right_fraction = 1 - left_fraction
         upper_bound_index = int(left_fraction * len(sorted_bootstrap_variations))
         lower_bound_index = int(right_fraction * len(sorted_bootstrap_variations))
 
