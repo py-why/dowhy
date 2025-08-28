7d83e228aebba91c6eb99da1ec13fef99deb05fd
Nicholas Parente <parentenickj@gmail.com>
2025-08-27 22:11:47 -0400

    add prop score clip arguments to docstring
    Signed-off-by: Nicholas Parente <parentenickj@gmail.com>

--- dowhy/causal_estimators/doubly_robust_estimator.py
+++ dowhy/causal_estimators/doubly_robust_estimator.py
@@ -71,6 +71,10 @@ class DoublyRobustEstimator(CausalEstimator):
             predict_proba() methods. If None, LogisticRegression is used
         :param propensity_score_column: Column name that stores the
             propensity score. Default='propensity_score'
+        :param min_ps_score: Lower bound used to clip the propensity score.
+            Default=0.05
+        :param max_ps_score: Upper bound used to clip the propensity score.
+            Default=0.95
         :param kwargs: (optional) Additional estimator-specific parameters
         """
         super().__init__(
